533d48ec08e81047c904afc74314a50a3f281efb
Roman Gonchar <roman.gonchar92@gmail.com>
2016-07-27 18:51:20 +0300

    Dev env db

--- environments/dev/common/config/db.php
+++ environments/dev/common/config/db.php
@@ -1,14 +1,14 @@
 <?php
 return [
     'db' => [
-        'dsn' => 'mysql:host=localhost;dbname=vents',
+        'dsn' => 'mysql:host=localhost;dbname=vipcms',
         'username' => 'root',
         'password' => '',
         'charset' => 'utf8',
         'tablePrefix' => 'fv_',
     ],
     'db-core' => [
-        'dsn' => 'mysql:host=localhost;dbname=vents',
+        'dsn' => 'mysql:host=localhost;dbname=vipcms',
         'username' => 'root',
         'password' => '',
         'charset' => 'utf8',
