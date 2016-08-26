778f5eb6dd2e370ec034f54a5d6639350c5fb458
FilamentV <vortex.filament>
2016-08-27 01:45:23 +0300

    console update

--- console/config/main.php
+++ console/config/main.php
@@ -1,34 +1,42 @@
 <?php
-return [
-    'id' => 'app-console',
-    'basePath' => dirname(__DIR__),
-    'runtimePath' => '@runtime/console',
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'console\controllers',
-    'modules' => require('modules.php'),
-    'components' => [
-        'log' => [
-            'targets' => [
-                [
-                    'class' => \yii\log\FileTarget::class,
-                    'levels' => ['error', 'warning'],
+use yii\helpers\ArrayHelper;
+
+$main = require(dirname(__DIR__, 2) . '/common/config/main.php');
+unset($main['bootstrap']['languages']);
+
+return ArrayHelper::merge(
+    $main,
+    [
+        'id' => 'app-console',
+        'basePath' => dirname(__DIR__),
+        'runtimePath' => '@runtime/console',
+        'bootstrap' => ['log'],
+        'controllerNamespace' => 'console\controllers',
+        'modules' => require('modules.php'),
+        'components' => [
+            'log' => [
+                'targets' => [
+                    [
+                        'class' => \yii\log\FileTarget::class,
+                        'levels' => ['error', 'warning'],
+                    ],
                 ],
             ],
         ],
-    ],
-    'controllerMap' => [
-        'migrate' => [
-            'class' => \thread\app\console\controllers\MigrateController::class,
-            'migrationPaths' => [
-                '@thread/modules/user/migrations',
-                '@thread/modules/location/migrations',
-                '@thread/modules/menu/migrations',
-                '@thread/modules/page/migrations',
-                '@thread/modules/news/migrations',
-                '@thread/modules/seo/migrations',
-                '@thread/modules/configs/migrations',
+        'controllerMap' => [
+            'migrate' => [
+                'class' => \thread\app\console\controllers\MigrateController::class,
+                'migrationPaths' => [
+                    '@thread/modules/user/migrations',
+                    '@thread/modules/location/migrations',
+                    '@thread/modules/menu/migrations',
+                    '@thread/modules/page/migrations',
+                    '@thread/modules/news/migrations',
+                    '@thread/modules/seo/migrations',
+                    '@thread/modules/configs/migrations',
+                ],
             ],
         ],
-    ],
-    'params' => [],
-];
+        'params' => [],
+    ]
+);
