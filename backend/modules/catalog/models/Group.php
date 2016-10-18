c8130387479b2bc225fddd5fc381c0732d7fce36
Andrii Bondarchuk <zndron@gmail.com>
2016-10-18 15:24:04 +0300

    upd

--- backend/modules/catalog/models/Group.php
+++ backend/modules/catalog/models/Group.php
@@ -26,15 +26,25 @@ class Group extends CommonGroupModel implements BaseBackendModel
         return ArrayHelper::map(self::findBase()->joinWith(['lang'])->all(), 'id', 'lang.title');
     }
 
-    public static function getDropdownListHierarchy() {
+    /**
+     * Backend form dropdown list hierarchy
+     * @return array
+     */
+    public static function getDropdownListHierarchy()
+    {
         $options = [];
 
-        $parents = self::findBase()->joinWith(['lang'])->where("parent_id=0")->all();
-        foreach($parents as $id => $p) {
-            $options[$p->id] = $p->lang->title;
-            $children = self::findBase()->joinWith(['lang'])->where("parent_id=:parent_id", [":parent_id"=>$p->id])->all();
-            foreach($children as $child) {
-                $options[$child->id] = ' - ' . $child->lang->title;
+        $parents = self::findBase()->innerJoinWith(['lang'])->where("parent_id=0")->all();
+
+        if ($parents != null) {
+            foreach ($parents as $p) {
+                $options[$p->id] = $p->lang->title;
+                $children = self::findBase()->innerJoinWith(['lang'])->where("parent_id=:parent_id", [":parent_id" => $p->id])->all();
+                if ($children != null) {
+                    foreach ($children as $child) {
+                        $options[$child->id] = ' - ' . $child->lang->title;
+                    }
+                }
             }
         }
         return $options;
