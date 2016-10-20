288b6569e8f737775a632e25de104a3cd31b3bd8
Alla Kuzmenko <myleto2011@gmail.com>
2016-10-20 15:46:09 +0300

    add file messeges to module shop

---
+++ thread/modules/shop/messages/ru/app.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @author FilamentV <vortex.filament@gmail.com>
+ * @copyright (c) 2015, Thread
+ * @version 09/04/2015
+ */
+return [
+    //module name
+    'Shop' => 'Магазин',
+    'Order' => 'Заказы',
+    'OrderItem' => 'Позиции в заказе',
+    'PaymentMethods' => 'Методы оплаты',
+    'DeliveryMethods' => 'Методы доставки',
+    //attribute
+    'manager_id' => 'Менеджер',
+    'user_id' => 'Покупатель',
+    'progress' => 'Прогресс',
+    'payd_status' => 'Оплата',
+    'count' => 'Количество',
+    'summ' => 'Итог',
+    'discount' => 'Скидка',
+    //payd status range
+    'ps_billed' => 'счет',
+    'ps_not_paid' => 'не оплачен',
+    'ps_paid_up' => 'оплачен',
+    //progress range
+    'p_new' => 'новый',
+    'p_confirmed' => 'подтвержденный',
+    'p_on_performance' => 'на выполнении',
+    'p_prepared' => 'подготовлен',
+    'p_on_delivery' => 'на доставке',
+    'p_refusal' => 'отклонен',
+    'p_executed' => 'выполенен',
+    //CartCustomerForm attribute
+    'ccf_name' => 'Ф.И.О.',
+    'ccf_email' => 'Email',
+    'ccf_phone' => 'Телефон',
+    'ccf_comment' => 'Адрес доставки',
+    'ccf_delivery' => 'Способ доставки',
+    'ccf_city' => 'Город',
+    'ccf_department' => 'Отделение',
+    'ccf_department_info' => 'Отделение доставки',
+];
