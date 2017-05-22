032178e05015484e9fcd2fd571c457369cc08df0
andrii bondarchuk <zndron@gmail.com>
2017-05-22 15:56:40 +0300

    upd

--- backend/themes/defaults/widgets/navbar/NavBar.php
+++ backend/themes/defaults/widgets/navbar/NavBar.php
@@ -71,62 +71,91 @@ class NavBar extends \yii\bootstrap\NavBar
     public $menuItems = [];
 
     /**
-     * Initializes the widget.
+     * Initializes the widget
      */
     public function init()
     {
         $this->registerTranslations();
-        $this->clientOptions = false;
-        $options = $this->options;
-        $tag = ArrayHelper::remove($options, 'tag', 'nav');
-        echo Html::beginTag($tag, $options);
-        echo Html::beginTag('div', $this->innerContainerOptions);
-        Html::addCssClass($this->containerOptions, ['nav' => 'nav', 'metismenu' => 'metismenu']);
-        $options = $this->containerOptions;
-        $tag = ArrayHelper::remove($options, 'tag', 'div');
-        echo Html::beginTag($tag, $options);
-    }
-
-    public function run()
-    {
-        echo $this->render('parts/_navbarHeader', ['bundle' => $this->bundle]);
 
         $modules = require(Yii::getAlias('@backend') . DIRECTORY_SEPARATOR . 'config' . '/modules.php');
 
         foreach ($modules as $moduleName => $moduleValue) {
             $moduleClass = Yii::$app->getModule($moduleName);
 
+            // get item menu
+            if (isset($moduleClass->menuItems)) {
+                $this->menuItems[$moduleName] = $this->getItem($moduleClass->menuItems);
+            }
+
+            // get items menu
             if (isset($moduleClass->menuItems) && key_exists('items', $moduleClass->menuItems)) {
                 $items = [];
                 foreach ($moduleClass->menuItems['items'] as $item) {
-                    $items[] = [
-                        'label' => '<i class="fa '.$item['icon'].'"></i><span class="nav-label">' .
-                            Yii::t('navbar', $item['name']) .
-                            '</span>',
-                        'url' => $item['url'],
-                    ];
+                    $items[] = $this->getItem($item);
                 }
-                $this->menuItems[] = [
-                    'label' => '<i class="fa '.$moduleClass->menuItems['icon'].'"></i><span class="nav-label">' .
-                        Yii::t('navbar', $moduleClass->menuItems['name']) .
-                        '</span>',
-                    'items' => $items,
-                    'position' => $moduleClass->menuItems['position'],
-                ];
-            } else if (isset($moduleClass->menuItems)) {
-                $this->menuItems[] = [
-                    'label' => '<i class="fa '.$moduleClass->menuItems['icon'].'"></i><span class="nav-label">' .
-                        Yii::t('navbar', $moduleClass->menuItems['name']) .
-                        '</span>',
-                    'url' => $moduleClass->menuItems['url'],
-                    'position' => $moduleClass->menuItems['position'],
-                ];
+                // set items menu and sort them
+                $this->menuItems[$moduleName]['items'] = $this->sortItems($items);
             }
         }
 
-        usort($this->menuItems, function ($item1, $item2) {
+        // sort items menu
+        $this->menuItems = $this->sortItems($this->menuItems);
+    }
+
+    /**
+     * Get item menu
+     *
+     * @param $item
+     * @return array
+     */
+    private function getItem($item)
+    {
+        $array = [];
+
+        $icon = $item['icon'] ?? '';
+        $name = Yii::t('navbar', $item['name']);
+
+        $array['label'] = '<i class="fa ' . $icon . '"></i><span class="nav-label">' . $name . '</span>';
+        if (isset($item['url'])) {
+            $array['url'] = $item['url'];
+        }
+        $array['position'] = $item['position'] ?? 0;
+
+        return $array;
+    }
+
+    /**
+     * Sort items menu
+     *
+     * @param $items
+     * @return mixed
+     */
+    private function sortItems($items)
+    {
+        usort($items, function ($item1, $item2) {
             return $item1['position'] <=> $item2['position'];
         });
+        return $items;
+    }
+
+    /**
+     * Run the widget
+     */
+    public function run()
+    {
+        $this->clientOptions = false;
+        $options = $this->options;
+        $tag = ArrayHelper::remove($options, 'tag', 'nav');
+
+        echo Html::beginTag($tag, $options);
+        echo Html::beginTag('div', $this->innerContainerOptions);
+        Html::addCssClass($this->containerOptions, ['nav' => 'nav', 'metismenu' => 'metismenu']);
+        $options = $this->containerOptions;
+        $tag = ArrayHelper::remove($options, 'tag', 'div');
+
+        echo Html::beginTag($tag, $options);
+
+        echo $this->render('parts/_navbarHeader', ['bundle' => $this->bundle]);
 
         echo Menu::widget([
             'items' => $this->menuItems,
