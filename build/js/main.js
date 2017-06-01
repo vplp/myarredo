d0169115f0b96a7be0ed83874265d26849ab1603
Yaroslav Rostanets <rostanets@gmail.com>
2017-06-01 18:57:21 +0300

    fix

--- build/js/main.js
+++ build/js/main.js
@@ -48,6 +48,8 @@ $(document).ready(function(){
         }
     });
 
+    /*
+
     $('.multi-item3-carousel .item').each(function(){
         var next = $(this).next();
         if (!next.length) {
@@ -63,5 +65,86 @@ $(document).ready(function(){
             $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
         }
     });
+    */
+
+    $('#rec-prod-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: '<a href=javascript:void(0) class="slick-prev fa fa-angle-left"></a>',
+        nextArrow: '<a href=javascript:void(0) class="slick-next fa fa-angle-right"></a>'
+    });
+
+    /*--Изменение вида в каталоге--*/
+    (function(){
+        $('.location-buts > a').click(function(){
+            $(this).siblings().removeClass('active');
+            $(this).addClass('active');
+            if($(this).attr('data-style')=='large'){
+                $(this).closest('.cont-area').addClass('large-tiles');
+            } else {
+                $(this).closest('.cont-area').removeClass('large-tiles');
+            }
+        });
+    })();
+    /*--конец Изменение вида в каталоге--*/
+
+    /*--Ползунок каталог--*/
+    (function(){
+        var slider = document.getElementById('price-slider');
+
+        if(slider != null){
+            noUiSlider.create(slider, {
+                start: [$('#min-price').val(),$('#max-price').val()],
+                connect: true,
+                animate: true,
+                animationDuration: 300,
+                range: {
+                    'min': Number($('#min-price').val()),
+                    'max': Number($('#max-price').val())
+                },
+                format: wNumb({
+                    decimals: 0
+                })
+
+            });
+
+            slider.noUiSlider.on('update',function(){
+                var arrResult = slider.noUiSlider.get();
+                $('#min-price').val(arrResult[0]);
+                $('#max-price').val(arrResult[1]);
+            });
+
+            $('#min-price').change(function(){
+                slider.noUiSlider.set([$(this).val(),$('#max-price').val()]);
+            });
+
+            $('#max-price').change(function(){
+                slider.noUiSlider.set([$('#min-price').val(),$(this).val()]);
+            });
+        }
+
+
+
+
+    })();
+    /*--конец ползунок--*/
+
+    /*--Открыть\закрыть категорию в каталоге--*/
+    (function () {
+        $('.filt-but').click(function(){
+            var parent = $(this).parent('.one-filter');
+                console.log(parent);
+                if(parent.hasClass('open')){
+                    parent.removeClass('open');
+                    parent.find('.list-item').slideUp();
+                } else {
+                    parent.addClass('open');
+                    parent.find('.list-item').slideDown();
+                }
+                //parent.find('.list-item').slideToggle();
+        });
+    })();
+    /*--конец Открыть\закрыть категорию в каталоге--*/
 
 });
