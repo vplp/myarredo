01e7e068dc937aa7f16fed539b533d793f991d0a
bogdan <vortex.filament@gmail.com>
2017-06-12 18:24:15 +0300

    ref

--- thread/app/console/controllers/MigrateController.php
+++ thread/app/console/controllers/MigrateController.php
@@ -99,19 +99,19 @@ class MigrateController extends \yii\console\controllers\MigrateController
 
         $migrations = [];
 
-        $handle = opendir(\Yii::getAlias($this->migrationPath));
-        while (($file = readdir($handle)) !== false) {
-            if ($file === '.' || $file === '..') {
-                continue;
-            }
-            $path = $this->migrationPath . DIRECTORY_SEPARATOR . $file;
-            if (preg_match('/^(m(\d{6}_\d{6})_.*?)\.php$/', $file,
-                    $matches) && !isset($applied[$matches[2]]) && is_file($path)
-            ) {
-                $migrations[] = $matches[1];
-            }
-        }
-        closedir($handle);
+//        $handle = opendir(\Yii::getAlias($this->migrationPath));
+//        while (($file = readdir($handle)) !== false) {
+//            if ($file === '.' || $file === '..') {
+//                continue;
+//            }
+//            $path = $this->migrationPath . DIRECTORY_SEPARATOR . $file;
+//            if (preg_match('/^(m(\d{6}_\d{6})_.*?)\.php$/', $file,
+//                    $matches) && !isset($applied[$matches[2]]) && is_file($path)
+//            ) {
+//                $migrations[] = $matches[1];
+//            }
+//        }
+//        closedir($handle);
 
         foreach ($this->migrationPaths as $path) {
             $migrations = ArrayHelper::merge($migrations, $this->getMigrationsByPath(\Yii::getAlias($path), $applied));
