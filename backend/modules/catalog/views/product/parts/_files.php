7a0e7e913d8e74e9e5239968526c697f13a5dff8
andrii bondarchuk <zndron@gmail.com>
2017-08-02 16:43:29 +0300

    upd

--- backend/modules/catalog/views/product/parts/_files.php
+++ backend/modules/catalog/views/product/parts/_files.php
@@ -1,6 +1,15 @@
 <?php
 /**
- *
- * @author Andrii Bondarchuk <zndron@gmail.com>
+ * @var \backend\modules\catalog\models\Product $model
+ * @var \backend\modules\catalog\models\ProductLang $modelLang
+ * @var \backend\themes\defaults\widgets\forms\ActiveForm $form
  */
- 
+
+?>
+
+<p>Прайсы и каталоги зависит от выбранной фабрики</p>
+
+
+<?php if ($model->factory_id): ?>
+
+<?php endif; ?>
