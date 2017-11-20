a66ca8b281b5d8500484b646c47081b67c0e5b14
andrii bondarchuk <zndron@gmail.com>
2017-11-20 13:51:31 +0200

    upd

--- console/config/main.php
+++ console/config/main.php
@@ -60,6 +60,9 @@ return ArrayHelper::merge(
                     "$rootDir/temp",
                 ],
             ],
+            'cron' => [
+                'class' => 'console\controllers\CronController',
+            ],
         ],
         'params' => [],
     ]
