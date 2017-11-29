cdfcd0fac9116bf2b1df819301f5c753af0e003f
andrii bondarchuk <zndron@gmail.com>
2017-11-29 18:13:37 +0200

    upd

--- frontend/modules/shop/mail/new_order_user_letter.php
+++ frontend/modules/shop/mail/new_order_user_letter.php
@@ -9,6 +9,69 @@ use frontend\modules\catalog\models\Product;
 
 ?>
 
+<div style="width:540px; height: 700px; font: 16px Arial,sans-serif;">
+    <div style="background:#c4c0b8 url('http://www.myarredo.ru/pic/letters/logo_note.png') center 10px no-repeat; height: 35px;  padding-top:45px; text-align:center;">
+        <span style="color: #fff; font:bold 16px Arial,sans-serif;">Мы помогаем купить итальянскую мебель по лучшим ценам.</span>
+    </div>
+    <div style="background-color:#fff9ea; text-align: left; padding: 10px 0 40px 10px;">
+        <p style="font-size: 16px;">Здравствуйте, <?= $order->customer['full_name'] ?></p>
+        <p style="font-size: 13px; color: #591612;">Ваш запрос <span style="font-weight: bold; color: #000;">№<?= $order['id'] ?></span> успешно принят.</p>
+        <p style="font-size: 14px; color: #000;">Вам прийдут письма с ценами на предметы, которые Вас заинтересовали от разных поставщиков.</p>
+        <p style="font-size: 14px; color: #000;">Вам нужно будет только выбрать лучшие условия для покупки.</p>
+        <p style="font-size: 14px; color: #000; margin-top:25px; ">Просчет занимает от 15 мнут до 1 рабочего дня.</p>
+    </div>
+
+    <div style="background-color:#fff; padding:20px; clear: both; display: block;">
+        <p style="color: #591612; font-size: 15px;">Запрошенные товары</p>
+
+        <?php foreach($order->items as $item): ?>
+            <div style="clear: both; height: 100px;">
+                <div style="float: left;">
+
+                    <?= Html::img(
+                            Product::getImageThumb($item->product->image_link),
+                            ['class' => 'width: 140px; max-height: 100px;']
+                    ); ?>
+
+                </div>
+                <div style="float: left; margin: 10px 30px;">
+
+                    <?= Html::a(
+                            $item->product['lang']['title'],
+                            Product::getUrl($item->product['alias']),
+                            ['style' => 'font-weight:bold; color: #000; text-transform: uppercase; text-decoration: underline;']
+                    ); ?>
+
+                    <br>
+                    <span style="color:#9f8b80; font-size: 14px;"><?= $item->product['factory']['lang']['title']; ?></span>
+                </div>
+            </div>
+        <?php endforeach; ?>
+
+    </div>
+
+<?php /*
+    <div style="background-color:#c4c0b8; padding:25px 60px 20px;">
+        <p style="color: #591612; margin-bottom: 1px;"><?php
+            echo (!empty($modelsTovarsOrderClient->partner))
+                ? $modelsTovarsOrderClient->partner->userData->address
+                : '';
+            ?></p>
+        <span style="display: block; color: #591612; margin-bottom: 5px;"><?php
+            echo (!empty($modelsTovarsOrderClient->partner))
+                ? $modelsTovarsOrderClient->partner->email
+                : '';
+            ?></span>
+        <span style="text-align: left; color: #0077ca; border-bottom: 1px dotted #0077ca;"><?php
+            echo (!empty($modelsTovarsOrderClient->partner))
+                ? $modelsTovarsOrderClient->partner->userData->telephone
+                : '';
+            ?></span>
+    </div>
+     */ ?>
+</div>
+
+<?php /*
 <p>Здравствуйте, <?= $order->customer['full_name'] ?></p>
 <p>Ваш запрос №<?= $order['id'] ?> успешно принят.</p>
 <p>Вам прийдут письма с ценами на предметы, которые Вас заинтересовали от разных поставщиков.</p>
@@ -24,3 +87,5 @@ use frontend\modules\catalog\models\Product;
 <?php endforeach; ?>
 
 <p>Для просмотра заказа пройдите по <a href="<?= $order->getTokenLink() ?>">ссылке</a>.</p>
+
+ */ ?>
