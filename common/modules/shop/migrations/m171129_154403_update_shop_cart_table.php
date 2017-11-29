3b571b5e50a475c255caa5ebb495664198eee2ac
andrii bondarchuk <zndron@gmail.com>
2017-11-29 17:50:10 +0200

    upd

---
+++ common/modules/shop/migrations/m171129_154403_update_shop_cart_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+use common\modules\shop\Shop;
+
+class m171129_154403_update_shop_cart_table extends Migration
+{
+    /**
+     * table name
+     * @var string
+     */
+    public $table = '{{%shop_cart}}';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->db = Shop::getDb();
+        parent::init();
+    }
+
+    /**
+     * Implement migration
+     */
+    public function safeUp()
+    {
+        $this->alterColumn($this->table, 'php_session_id', $this->string(180)->notNull()->comment('Session ID'));
+
+    }
+
+    /**
+     * Cancel migration
+     */
+    public function safeDown()
+    {
+        $this->alterColumn($this->table, 'php_session_id', $this->string(30)->notNull()->comment('Session ID'));
+
+    }
+}
