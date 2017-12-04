08b66515ccb1c487baf3f785f29a1210f861b480
andrii bondarchuk <zndron@gmail.com>
2017-12-04 17:45:51 +0200

    upd

--- backend/modules/catalog/views/sale/parts/_image.php
+++ backend/modules/catalog/views/sale/parts/_image.php
@@ -1,1 +1,13 @@
-в разработке
+<?php
+
+/**
+ * @var \backend\modules\catalog\models\Product $model
+ * @var \backend\modules\catalog\models\ProductLang $modelLang
+ * @var \backend\app\bootstrap\ActiveForm $form
+ */
+
+?>
+
+<?= $form->field($model, 'image_link')->imageOne($model->getImageLink()) ?>
+
+<?= $form->field($model, 'gallery_image')->imageSeveral(['initialPreview' => $model->getGalleryImage()]) ?>
