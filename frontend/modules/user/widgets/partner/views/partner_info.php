658ba67960963fb143d4171c41940a3d66a1ca28
andrii bondarchuk <zndron@gmail.com>
2017-12-21 18:23:06 +0200

    upd

--- frontend/modules/user/widgets/partner/views/partner_info.php
+++ frontend/modules/user/widgets/partner/views/partner_info.php
@@ -7,7 +7,11 @@ use yii\helpers\Html;
 <meta itemprop="name" content="<?= Html::encode($partner['profile']['name_company']) ?>"/>
 
 <div class="cons">Получить консультацию в <?= $city['lang']['title_where'] ?></div>
+
+<?php if (Yii::$app->controller->id !== 'sale'): ?>
 <div class="tel" itemprop="telephone"><i class="fa fa-phone" aria-hidden="true"></i><?= $partner['profile']['phone'] ?></div>
+<?php endif; ?>
+
 <div class="stud" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
     <meta itemprop="addressLocality" content="<?= $city['country']['lang']['title'] ?> <?= $city['lang']['title'] ?>"/>
     <meta itemprop="streetAddress" content="<?= $partner['profile']['address'] ?>"/>
