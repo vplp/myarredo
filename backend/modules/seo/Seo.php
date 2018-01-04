6c58fc84ca9eec6907456c50e0d1bac3d29132d0
andrii bondarchuk <zndron@gmail.com>
2018-01-04 12:07:43 +0200

    upd

--- backend/modules/seo/Seo.php
+++ backend/modules/seo/Seo.php
@@ -44,21 +44,21 @@ class Seo extends \common\modules\seo\Seo
                 'position' => 10,
                 'items' =>
                     [
-                        [
+                        /*[
                             'name' => 'Robots.txt',
                             'position' => 1,
                             'url' => ['/seo/robots/update'],
-                        ],
+                        ],*/
                         [
                             'name' => 'Direct Link',
                             'position' => 2,
                             'url' => ['/seo/directlink/directlink/list'],
                         ],
-                        [
+                        /*[
                             'name' => 'Base Info',
                             'position' => 2,
                             'url' => ['/seo/info/info/list'],
-                        ]
+                        ]*/
                     ]
             ];
         }
--- backend/modules/seo/modules/directlink/views/directlink/list.php
+++ backend/modules/seo/modules/directlink/views/directlink/list.php
@@ -6,13 +6,12 @@ use thread\widgets\grid\{
     ActionStatusColumn
 };
 
-//
 echo GridView::widget([
     'dataProvider' => $model->search(Yii::$app->request->queryParams),
     'filterModel' => $filter,
     'columns' => [
         'url',
-        [
+        /*[
             'class' => ActionStatusColumn::class,
             'attribute' => 'add_to_sitemap',
             'action' => 'add_to_sitemap'
@@ -21,7 +20,7 @@ echo GridView::widget([
             'class' => ActionStatusColumn::class,
             'attribute' => 'dissallow_in_robotstxt',
             'action' => 'dissallow_in_robotstxt'
-        ],
+        ],*/
         [
             'class' => ActionStatusColumn::class,
             'attribute' => 'published',
