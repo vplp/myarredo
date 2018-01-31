46c3edd2acd2b46a44a1c827e34664f86560e785
Yaroslav <rostanets@gmail.com>
2018-01-31 13:13:41 +0200

    fix styles

--- frontend/themes/myarredo/app/public/js/core.js
+++ frontend/themes/myarredo/app/public/js/core.js
@@ -462,10 +462,23 @@ $(document).ready(function(){
 
 });
 (function(){
+    var loaderTemplate = `<div class="loader" style="display: none;">
+    <div id="floatingCirclesG">
+        <div class="f_circleG" id="frotateG_01"></div>
+        <div class="f_circleG" id="frotateG_02"></div>
+        <div class="f_circleG" id="frotateG_03"></div>
+        <div class="f_circleG" id="frotateG_04"></div>
+        <div class="f_circleG" id="frotateG_05"></div>
+        <div class="f_circleG" id="frotateG_06"></div>
+        <div class="f_circleG" id="frotateG_07"></div>
+        <div class="f_circleG" id="frotateG_08"></div>
+    </div>
+</div>`;
+
     if( $("#checkout-form").length ){
 
         var btn = $("#checkout-form button[type=submit]");
-
+        $('body').append(loaderTemplate);
         $("#checkout-form").on("submit",function(e){
             btn.addClass("disabled");
             $(".loader").show();
