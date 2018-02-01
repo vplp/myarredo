d6710a092ccd6c9188ce1be1726132946f42df88
andrii bondarchuk <zndron@gmail.com>
2018-02-01 10:50:38 +0200

    upd

--- frontend/modules/user/widgets/partner/views/partner_info.php
+++ frontend/modules/user/widgets/partner/views/partner_info.php
@@ -4,7 +4,10 @@ use yii\helpers\Html;
 
 ?>
 
-<?php if (!Yii::$app->getUser()->isGuest && in_array(Yii::$app->getUser()->getIdentity()->group->role, ['partner', 'admin', 'factory'])): ?>
+<?php if (
+    !Yii::$app->getUser()->isGuest
+    && in_array(Yii::$app->getUser()->getIdentity()->group->role, ['partner', 'admin', 'factory'])
+): ?>
 
     <div class="cons">Администрация проекта</div>
 
@@ -25,20 +28,19 @@ use yii\helpers\Html;
         <i class="fa" aria-hidden="true"></i>skype: <?= Html::a('myarredo', 'skype:myarredo?chat') ?>
     </div>
 
-<?php else: ?>
+<?php elseif (Yii::$app->controller->id !== 'sale'): ?>
 
     <meta itemprop="name" content="<?= Html::encode($partner['profile']['name_company']) ?>"/>
 
     <div class="cons">Получить консультацию в <?= $city['lang']['title_where'] ?></div>
 
-    <?php if (Yii::$app->controller->id !== 'sale'): ?>
-        <div class="tel" itemprop="telephone">
-            <i class="fa fa-phone" aria-hidden="true"></i><?= $partner['profile']['phone'] ?>
-        </div>
-    <?php endif; ?>
+    <div class="tel" itemprop="telephone">
+        <i class="fa fa-phone" aria-hidden="true"></i><?= $partner['profile']['phone'] ?>
+    </div>
 
     <div class="stud" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-        <meta itemprop="addressLocality" content="<?= $city['country']['lang']['title'] ?> <?= $city['lang']['title'] ?>"/>
+        <meta itemprop="addressLocality"
+              content="<?= $city['country']['lang']['title'] ?> <?= $city['lang']['title'] ?>"/>
         <meta itemprop="streetAddress" content="<?= $partner['profile']['address'] ?>"/>
         <?= $partner['profile']['name_company'] ?><br><?= $partner['profile']['address'] ?>
     </div>
