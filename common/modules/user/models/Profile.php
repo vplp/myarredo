ab129b9352462551684da94cc1d5a061dba48a9c
andrii bondarchuk <zndron@gmail.com>
2018-02-16 11:09:15 +0200

    upd

--- common/modules/user/models/Profile.php
+++ common/modules/user/models/Profile.php
@@ -267,9 +267,13 @@ class Profile extends \thread\modules\user\models\Profile
     {
         $ch = curl_init();
         $timeout = 5;
+
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
         $data = curl_exec($ch);
         curl_close($ch);
 
