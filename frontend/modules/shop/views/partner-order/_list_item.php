d54a1cd427c01f9da7d9c0ee5b6c2b5ed3b57448
andrii bondarchuk <zndron@gmail.com>
2018-03-06 10:37:05 +0200

    upd

--- frontend/modules/shop/views/partner-order/_list_item.php
+++ frontend/modules/shop/views/partner-order/_list_item.php
@@ -120,17 +120,23 @@ if (Yii::$app->user->identity->profile->possibilityToAnswer) { ?>
     </div>
 
     <?php
-    if (Yii::$app->user->identity->profile->getPossibilityToSaveAnswer($modelOrder->city_id) != null && (!$modelOrderAnswer->id || $modelOrderAnswer->answer_time == 0)) {
+    if (
+        Yii::$app->user->identity->profile->getPossibilityToSaveAnswer($modelOrder->city_id) != null &&
+        (!$modelOrderAnswer->id || $modelOrderAnswer->answer_time == 0)
+    ) {
         echo Html::submitButton('Сохранить', [
             'class' => 'btn btn-success action-save-answer',
             'name' => 'action-save-answer',
             'value' => 1
         ]);
     } else {
-      echo '<p>Оплатите возможность отвечать на заявки из этого города!</p>';
+        echo '<p>Оплатите возможность отвечать на заявки из этого города!</p>';
     } ?>
 
-    <?php if (Yii::$app->user->identity->profile->>getPossibilityToSaveAnswer($modelOrder->city_id) != null && ($modelOrderAnswer->id && $modelOrderAnswer->answer_time == 0)) {
+    <?php if (
+        Yii::$app->user->identity->profile->getPossibilityToSaveAnswer($modelOrder->city_id) != null &&
+        ($modelOrderAnswer->id && $modelOrderAnswer->answer_time == 0)
+    ) {
         echo Html::submitButton('Отправить ответ клиенту', [
             'class' => 'btn btn-success',
             'name' => 'action-send-answer',
