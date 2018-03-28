5231c423967cbfcf782f367e8e1c0538448bdc8a
andrii bondarchuk <zndron@gmail.com>
2018-03-28 15:54:16 +0300

    upd

--- frontend/modules/user/controllers/RegisterController.php
+++ frontend/modules/user/controllers/RegisterController.php
@@ -170,11 +170,7 @@ class RegisterController extends BaseController
                     ->setTo(Yii::$app->params['mailer']['setTo'])
                     ->setSubject('Зарегистрирована новая фабрика')
                     ->send();
-            }
-
-            if ($status === true) {
                 Yii::$app->getSession()->addFlash('login', Yii::$app->param->getByName('USER_FACTORY_REG_MESSAGE'));
-                return $this->redirect(Url::toRoute('/user/login/index'));
             }
         }
 
