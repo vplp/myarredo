2ecc31ebfc79b6d8a46711ac3997ab4e2f475aef
Yaroslav <rostanets@gmail.com>
2018-04-21 19:19:15 +0300

    fix

--- frontend/modules/page/views/contacts/contacts.php
+++ frontend/modules/page/views/contacts/contacts.php
@@ -18,14 +18,40 @@ $bundle = AppAsset::register($this);
                 <div class="col-md-12">
                     <?= Html::tag('h1', $this->context->title); ?>
                     <div class="of-conts">
-
+                        <?php
+                            $mainPartner = array_shift($partners);
+                        ?>
+                        <div class="one-cont double-cont">
+                            <div class="img-part">
+                                <img src="<?= $bundle->baseUrl ?>/img/cont1.svg" alt="">
+                            </div>
+                            <div class="info-part">
+                                <div class="main-sticker">
+                                    Главный партнер
+                                </div>
+                                <?= Html::tag('h4', $mainPartner->profile->name_company); ?>
+                                <div class="ico">
+                                    <img src="<?= $bundle->baseUrl ?>/img/phone.svg" alt="">
+                                </div>
+                                <a href="tel:<?= $mainPartner->profile->getPhone() ?>"><?= $mainPartner->profile->getPhone() ?></a>
+                                <div class="ico">
+                                    <img src="<?= $bundle->baseUrl ?>/img/marker-map.png" alt="">
+                                </div>
+                                <div class="adres">
+                                    <?= $mainPartner->profile->address ?>
+                                </div>
+                            </div>
+                        </div>
                         <?php foreach ($partners as $partner): ?>
                             <div class="one-cont">
                                 <?= Html::tag('h4', $partner->profile->name_company); ?>
                                 <div class="ico">
-                                    <img src="<?= $bundle->baseUrl ?>/img/sign-in1.svg" alt="">
+                                    <img src="<?= $bundle->baseUrl ?>/img/phone.svg" alt="">
                                 </div>
                                 <a href="tel:<?= $partner->profile->getPhone() ?>"><?= $partner->profile->getPhone() ?></a>
+                                <div class="ico">
+                                    <img src="<?= $bundle->baseUrl ?>/img/marker-map.png" alt="">
+                                </div>
                                 <div class="adres">
                                     <?= $partner->profile->address ?>
                                 </div>
@@ -36,17 +62,7 @@ $bundle = AppAsset::register($this);
                     <div class="warning">
                         * <?= Yii::t('app','Обращаем ваше внимание, цены партнеров сети могут отличаться.') ?>)
                     </div>
-                    <div class="map-cont">
 
-                        <?= PartnerMap::widget(['city' => Yii::$app->city->getCity()]) ?>
-
-                        <?= Html::a(
-                            Yii::t('app','Посмотреть все офисы продаж'),
-                            Url::toRoute('/page/contacts/list-partners'),
-                            ['class' => 'view-all']
-                        ); ?>
-
-                    </div>
                 </div>
 
             </div>
