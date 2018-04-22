595fc4318349ffad2c1af3a13a7752b889842f52
Yaroslav <rostanets@gmail.com>
2018-04-22 22:32:36 +0300

    fix styles

--- frontend/modules/catalog/views/factory/view.php
+++ frontend/modules/catalog/views/factory/view.php
@@ -4,6 +4,7 @@ use yii\helpers\{
     Url, Html
 };
 //
+use frontend\themes\myarredo\assets\AppAsset;
 use frontend\components\Breadcrumbs;
 use frontend\modules\catalog\models\Factory;
 
@@ -15,209 +16,208 @@ $keys = Yii::$app->catalogFilter->keys;
 
 $this->title = $this->context->title;
 
+$bundle = AppAsset::register($this);
+
 ?>
 
 <main>
     <div class="page factory-page">
-        <div class="letter-nav">
+        <div class="container-wrap">
             <div class="container large-container">
-                <ul class="letter-select">
 
-                    <?php foreach (Factory::getListLetters() as $letter) {
-                        echo Html::beginTag('li') .
-                            Html::a(
-                                $letter['first_letter'],
-                                Url::toRoute(
-                                    ['/catalog/factory/list', 'letter' => strtolower($letter['first_letter'])]
-                                )
-                            ) .
-                            Html::endTag('li');
-                    } ?>
-
-                </ul>
-                <?= Html::a('Все', Url::toRoute(['/catalog/factory/list']), ['class' => 'all']); ?>
-            </div>
-        </div>
-        <div class="container large-container">
-
-            <div class="row">
-                <?= Breadcrumbs::widget([
-                    'links' => $this->context->breadcrumbs,
-                ]) ?>
-            </div>
-
-            <div class="row factory-det">
-                <div class="col-sm-3 col-md-3">
-                    <div class="fact-img">
-                        <?= Html::img(Factory::getImage($model['image_link'])); ?>
-                    </div>
+                <div class="row">
+                    <?= Breadcrumbs::widget([
+                        'links' => $this->context->breadcrumbs,
+                    ]) ?>
                 </div>
-                <div class="col-sm-9 col-md-9">
-                    <div class="descr">
 
-                        <?= Html::tag(
-                            'h1',
-                            (($this->context->SeoH1)
-                                ? $this->context->SeoH1
-                                : Yii::t('app','Мебель') . ' ' . $model['title'] . ' ' . Yii::t('app','в') . ' ' . Yii::$app->city->getCityTitleWhere()),
-                            ['class' => 'title-text']
-                        ); ?>
+                <div class="row factory-det">
+                    <div class="col-xs-12 col-sm-4 col-md-3">
+                        <div class="fact-img">
+                            <?= Html::img(Factory::getImage($model['image_link'])); ?>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-8 col-md-9">
+                        <div class="descr">
 
-                        <div class="fact-link">
-                            <?= Html::a(
-                                $model['url'],
-                                'http://' . $model['url'],
+                            <?= Html::tag(
+                                'h1',
+                                (($this->context->SeoH1)
+                                    ? $this->context->SeoH1
+                                    : Yii::t('app','Мебель') . ' ' . $model['title'] . ' ' . Yii::t('app','в') . ' ' . Yii::$app->city->getCityTitleWhere()),
+                                ['class' => 'title-text']
+                            ); ?>
+
+                            <div class="fact-link">
+                                <?= Html::a(
+                                    $model['url'],
+                                    'http://' . $model['url'],
+                                    [
+                                        'target' => '_blank',
+                                        'rel' => 'nofollow'
+                                    ]
+                                ); ?>
+                            </div>
+
+                            <div class="fact-assort-wrap">
+                                <ul class="nav nav-tabs">
+                                    <li class="active">
+                                        <a data-toggle="tab" href="#all-product">
+                                            <?= Yii::t('app','Все предметы мебели') ?>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a data-toggle="tab" href="#all-collection">
+                                            <?= Yii::t('app','Все коллекции') ?>
+                                        </a>
+                                    </li>
+                                </ul>
+
+                                <div class="tab-content">
+                                    <div id="all-product" class="tab-pane fade in active">
+                                        <ul class="list">
+                                            <?php
+                                            $FactoryTypes = Factory::getFactoryTypes($model['id']);
+
+                                            foreach ($FactoryTypes as $item) {
+
+                                                $params = Yii::$app->catalogFilter->params;
+
+                                                $params[$keys['factory']][] = $model['alias'];
+                                                $params[$keys['type']][] = $item['alias'];
+
+                                                echo Html::beginTag('li') .
+                                                    Html::a(
+                                                        $item['title'] . ' <span>' . $item['count'] . '</span>',
+                                                        Yii::$app->catalogFilter->createUrl($params)
+                                                    ) .
+                                                    Html::endTag('li');
+
+                                            }
+                                            ?>
+                                        </ul>
+                                    </div>
+                                    <div id="all-collection" class="tab-pane fade">
+                                        <ul class="list">
+                                            <?
+
+                                            $FactoryCollection = Factory::getFactoryCollection($model['id']);
+
+                                            foreach ($FactoryCollection as $item) {
+
+                                                $params = Yii::$app->catalogFilter->params;
+
+                                                $params[$keys['factory']][] = $model['alias'];
+                                                $params[$keys['collection']][] = $item['id'];
+
+                                                echo Html::beginTag('li') .
+                                                    Html::a(
+                                                        $item['title'] . ' <span>' . $item['count'] . '</span>',
+                                                        Yii::$app->catalogFilter->createUrl($params)
+                                                    ) .
+                                                    Html::endTag('li');
+
+                                            }
+                                            ?>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <?= $this->render(
+                                'parts/_factory_files',
                                 [
-                                    'target' => '_blank',
-                                    'rel' => 'nofollow'
+                                    'model' => $model
                                 ]
                             ); ?>
+
                         </div>
+                    </div>
+                </div>
 
-                        <div class="fact-assort">
-                            <div class="all-list">
-                                <a href="#" class="title"><?= Yii::t('app','Все предметы мебели') ?></a>
-                                <ul class="list">
-
-                                    <?php
-                                    $key = 1;
-                                    $FactoryTypes = Factory::getFactoryTypes($model['id']);
-                                    foreach ($FactoryTypes as $item) {
-
-                                        $params = Yii::$app->catalogFilter->params;
-
-                                        $params[$keys['factory']][] = $model['alias'];
-                                        $params[$keys['type']][] = $item['alias'];
-
-                                        echo Html::beginTag('li') .
-                                            Html::a(
-                                                $item['title'] . ' (' . $item['count'] . ')',
-                                                Yii::$app->catalogFilter->createUrl($params)
-                                            ) .
-                                            Html::endTag('li');
-
-                                        if ($key == 10) {
-                                            echo '</ul><ul class="list post-list">';
-                                        }
-
-                                        ++$key;
-
-                                    } ?>
-
-                                </ul>
-
-                                <?php
-                                if (count($FactoryTypes) > 10) {
-                                    echo Html::a(
-                                        Yii::t('app','Весь список'),
-                                        'javascript:void(0);',
-                                        ['class' => 'view-all']
-                                    );
-                                } ?>
-
-                            </div>
-                            <div class="all-list">
-                                <a href="#" class="title">
-                                    <?= Yii::t('app','Все коллекции') ?>
-                                </a>
-                                <ul class="list">
-
-                                    <?php
-                                    $key = 1;
-                                    $FactoryCollection = Factory::getFactoryCollection($model['id']);
-                                    foreach ($FactoryCollection as $item) {
-
-                                        $params = Yii::$app->catalogFilter->params;
-
-                                        $params[$keys['factory']][] = $model['alias'];
-                                        $params[$keys['collection']][] = $item['id'];
-
-                                        echo Html::beginTag('li') .
-                                            Html::a(
-                                                $item['title'] . ' (' . $item['count'] . ')',
-                                                Yii::$app->catalogFilter->createUrl($params)
-                                            ) .
-                                            Html::endTag('li');
-
-                                        if ($key == 10) {
-                                            echo '</ul><ul class="list post-list">';
-                                        }
-
-                                        ++$key;
-
-                                    } ?>
-
-                                </ul>
-
-                                <?php
-                                if (count($FactoryCollection) > 10) {
-                                    echo Html::a(
-                                        Yii::t('app','Весь список'),
-                                        'javascript:void(0);',
-                                        ['class' => 'view-all']
-                                    );
-                                } ?>
-
+                <div class="row factory-text">
+                    <div class="col-xs-12 col-sm-4 col-md-3">
+                        <div class="header-border">
+                            <div class="text-header">
+                                В ассортименте представлена
+                                мебель для наиболее
+                                значимых и важных зон
+                                каждого дома
                             </div>
                         </div>
-
-                        <?= $this->render(
-                            'parts/_factory_files',
-                            [
-                                'model' => $model
-                            ]
-                        ); ?>
-
+                    </div>
+                    <div class="col-xs-12 col-sm-8 col-md-9">
                         <div class="text">
                             <?= $model['lang']['content']; ?>
                         </div>
-
                     </div>
                 </div>
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4 col-md-3">
+                        <div class="one-filter">
+                            <h4>
+                                Категории
+                            </h4>
+                            <ul class="list">
+                                <?php
+                                $key = 1;
+                                $FactoryCategory = Factory::getFactoryCategory([$model['id']]);
+
+                                foreach ($FactoryCategory as $item) {
+                                    $params = Yii::$app->catalogFilter->params;
+
+                                    $params[$keys['factory']][] = $model['alias'];
+                                    $params[$keys['category']][] = $item['alias'];
+
+                                    ?>
+                                    <li>
+                                        <a href="<?=Yii::$app->catalogFilter->createUrl($params)?>">
+                                            <div class="left-group">
+                                                <div class="img-cont">
+                                                    <img src="<?= $bundle->baseUrl ?>/img/catalog_ico/thumb1.png" alt="">
+                                                </div>
+                                                <?= $item['title'] ?>
+                                            </div>
+                                            <span class="count">
+                                                12707
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <?php
+                                }
+                                ?>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-8 col-md-9">
+                        <div class="cat-prod catalog-wrap">
+
+                            <?php foreach ($product as $item) {
+                                echo $this->render('/category/_list_item', [
+                                    'model' => $item,
+                                    'factory' => [$model->id => $model]
+                                ]);
+                            }
+                            ?>
+
+                            <div class="one-prod-tile last">
+                                <div class="img-cont">
+                                    <img src="<?= $bundle->baseUrl ?>/img/factory.svg" alt="">
+                                </div>
+                                <a class="view-all"
+                                   href="<?=Yii::$app->catalogFilter->createUrl(Yii::$app->catalogFilter->params + [$keys['factory'] => $model['alias']])?>">
+                                    Смотреть полный каталог
+                                </a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <?= $this->context->SeoContent ?>
+
             </div>
-
-            <div class="row menu-style">
-                <a href="#">Все</a>
-
-                <?php
-                $key = 1;
-                $FactoryCategory = Factory::getFactoryCategory([$model['id']]);
-
-                foreach ($FactoryCategory as $item) {
-                    $params = Yii::$app->catalogFilter->params;
-
-                    $params[$keys['factory']][] = $model['alias'];
-                    $params[$keys['category']][] = $item['alias'];
-
-                    echo Html::a(
-                            $item['title'],
-                            Yii::$app->catalogFilter->createUrl($params)
-                        );
-                } ?>
-
-            </div>
-
-            <div class="cat-prod catalog-wrap">
-
-                <?php foreach ($product as $item) {
-                    echo $this->render('/category/_list_item', [
-                        'model' => $item,
-                        'factory' => [$model->id => $model]
-                    ]);
-                }
-
-                echo Html::a(
-                    Yii::t('app','смотреть полный<div>Каталог</div>'),
-                    Yii::$app->catalogFilter->createUrl(
-                        Yii::$app->catalogFilter->params + [$keys['factory'] => $model['alias']]
-                    ),
-                    ['class' => 'one-prod-tile last']
-                ); ?>
-
-            </div>
-
-            <?= $this->context->SeoContent ?>
-
         </div>
     </div>
 </main>
