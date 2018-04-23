7e11058850d9896facce643dbf9e981a908c02d3
andrii bondarchuk <zndron@gmail.com>
2018-04-23 16:33:10 +0300

    upd

--- frontend/modules/seo/controllers/RobotsController.php
+++ frontend/modules/seo/controllers/RobotsController.php
@@ -18,30 +18,33 @@ class RobotsController extends Controller
     public function actionIndex()
     {
         echo 'User-agent: *' . PHP_EOL;
-        echo 'Disallow: *?view' . PHP_EOL;
-        echo 'Disallow: /partner/' . PHP_EOL;
-        echo 'Disallow: /*openstat*' . PHP_EOL;
-        echo 'Disallow: /*utm*' . PHP_EOL;
-        echo 'Disallow: /search/' . PHP_EOL;
-        echo 'Disallow: /orders/' . PHP_EOL;
-        echo 'Disallow: *three=*' . PHP_EOL;
-        echo 'Disallow: *sort=*' . PHP_EOL;
-        echo 'Disallow: *object=*' . PHP_EOL;
-        echo 'Disallow: *partner=*' . PHP_EOL;
-        echo 'Disallow: *?three*' . PHP_EOL;
-        echo 'Disallow: /type/' . PHP_EOL;
-        echo 'Disallow: /style/' . PHP_EOL;
-        echo 'Disallow: *login*' . PHP_EOL;
-        echo 'Disallow: *notepad*' . PHP_EOL;
-        echo 'Disallow: *=novelty*' . PHP_EOL;
-        echo 'Disallow: *=bestseller*' . PHP_EOL;
-        echo 'Disallow: /catalog/*?' . PHP_EOL;
 
-        echo PHP_EOL . 'Host: ' . Yii::$app->request->serverName . PHP_EOL;
+        echo 'Disallow: /' . PHP_EOL;
 
-        $city = Yii::$app->city->getCity();
-
-        echo 'Sitemap: http://' . Yii::$app->request->hostName . '/sitemap/sitemap_' . $city->alias . '.xml' . PHP_EOL;
+//        echo 'Disallow: *?view' . PHP_EOL;
+//        echo 'Disallow: /partner/' . PHP_EOL;
+//        echo 'Disallow: /*openstat*' . PHP_EOL;
+//        echo 'Disallow: /*utm*' . PHP_EOL;
+//        echo 'Disallow: /search/' . PHP_EOL;
+//        echo 'Disallow: /orders/' . PHP_EOL;
+//        echo 'Disallow: *three=*' . PHP_EOL;
+//        echo 'Disallow: *sort=*' . PHP_EOL;
+//        echo 'Disallow: *object=*' . PHP_EOL;
+//        echo 'Disallow: *partner=*' . PHP_EOL;
+//        echo 'Disallow: *?three*' . PHP_EOL;
+//        echo 'Disallow: /type/' . PHP_EOL;
+//        echo 'Disallow: /style/' . PHP_EOL;
+//        echo 'Disallow: *login*' . PHP_EOL;
+//        echo 'Disallow: *notepad*' . PHP_EOL;
+//        echo 'Disallow: *=novelty*' . PHP_EOL;
+//        echo 'Disallow: *=bestseller*' . PHP_EOL;
+//        echo 'Disallow: /catalog/*?' . PHP_EOL;
+//
+//        echo PHP_EOL . 'Host: ' . Yii::$app->request->serverName . PHP_EOL;
+//
+//        $city = Yii::$app->city->getCity();
+//
+//        echo 'Sitemap: http://' . Yii::$app->request->hostName . '/sitemap/sitemap_' . $city->alias . '.xml' . PHP_EOL;
 
         $response = Yii::$app->response;
         $response->format = yii\web\Response::FORMAT_RAW;
