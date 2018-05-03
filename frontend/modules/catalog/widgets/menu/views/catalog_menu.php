0dab743a2c7acca10a1ac0e75dcd1fb8548eb8a5
andrii bondarchuk <zndron@gmail.com>
2018-05-03 12:19:44 +0300

    upd

--- frontend/modules/catalog/widgets/menu/views/catalog_menu.php
+++ frontend/modules/catalog/widgets/menu/views/catalog_menu.php
@@ -22,7 +22,7 @@ use frontend\modules\catalog\models\Category;
                     <li>
                         <a href="<?= Category::getUrl($model['alias']) ?>">
                             <div class="img-cont">
-                                <?= Html::img(Category::getImageThumb($model['image_link'])); ?>
+                                <?= Html::img(Category::getImage($model['image_link3'])); ?>
                             </div>
                             <?= $model['lang']['title'] ?>
                         </a>
