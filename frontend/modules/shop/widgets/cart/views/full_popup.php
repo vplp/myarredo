b875d921e088d8033d20b49c6bd966baf5df63b0
Vados <javadosservice@gmail.com>
2018-07-10 13:45:46 +0300

    add style for popup cart

--- frontend/modules/shop/widgets/cart/views/full_popup.php
+++ frontend/modules/shop/widgets/cart/views/full_popup.php
@@ -7,21 +7,18 @@ use yii\helpers\{
 ?>
 
 <div class="modal-dialog" role="document">
-    <div class="modal-content">
+    <div class="modal-content cart-modal">
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                         aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" id="myModalLabel">Добавление предмета в блокнот</h4>
         </div>
         <p>Предмет добавлен в блокнот</p>
-        <!--<div class="modal-body">
-            <?php foreach ($items as $item): ?>
-                <?= $this->render('parts/item_of_cart_popup', ['item' => $item, 'product' => $products[$item['product_id']] ?? []]); ?>
-            <?php endforeach; ?>
-        </div>-->
+        <div class="modal-body">
+            <p> Предмет добавлен в мои желания </p>
+        </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Продолжить покупки</button>
-            <?= Html::a('Перейти в блокнот', ['/shop/cart/notepad'], ['class' => 'btn btn-primary']) ?>
+            <button type="button" class="btn btn-cancel" data-dismiss="modal">Продолжить покупки</button>
+            <?= Html::a('Перейти в блокнот', ['/shop/cart/notepad'], ['class' => 'btn btn-goods']) ?>
         </div>
     </div>
 </div>
