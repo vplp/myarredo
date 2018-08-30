6d91daa525ad35805fa02988fda6ee3f05cfd8c8
andrii bondarchuk <zndron@gmail.com>
2018-08-30 18:02:52 +0300

    upd

--- frontend/components/BaseController.php
+++ frontend/components/BaseController.php
@@ -40,12 +40,41 @@ abstract class BaseController extends Controller
     }
 
     /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [
+//            [
+//                'class' => 'yii\filters\HttpCache',
+//                'only' => ['index'],
+//                'lastModified' => function ($action, $params) {
+//                    $q = new \yii\db\Query();
+//                    return $q->from('post')->max('updated_at');
+//                },
+//            ],
+            [
+                'class' => 'yii\filters\HttpCache',
+                'lastModified' => function ($action, $params) {
+                    return time();
+                },
+                'sessionCacheLimiter' => 'public',
+                //'cacheControlHeader' => 'public, max-age=3600', // not needed since it is the default value
+            ],
+        ];
+    }
+
+    /**
      * @return string
      */
     public function getSeoH1()
     {
         if (isset($this->directLink['lang']) && $this->directLink['lang']['h1']) {
-            $this->pageH1 = str_replace(['#городе#', '#nella citta#'], Yii::$app->city->getCityTitleWhere(), $this->directLink['lang']['h1']);
+            $this->pageH1 = str_replace(
+                ['#городе#', '#nella citta#'],
+                Yii::$app->city->getCityTitleWhere(),
+                $this->directLink['lang']['h1']
+            );
         }
 
         return $this->pageH1;
@@ -59,7 +88,11 @@ abstract class BaseController extends Controller
         $content = false;
 
         if (isset($this->directLink['lang']) && $this->directLink['lang']['content']) {
-            $content = str_replace(['#городе#', '#nella citta#'], Yii::$app->city->getCityTitleWhere(), $this->directLink['lang']['content']);
+            $content = str_replace(
+                ['#городе#', '#nella citta#'],
+                Yii::$app->city->getCityTitleWhere(),
+                $this->directLink['lang']['content']
+            );
         }
 
         return $content;
