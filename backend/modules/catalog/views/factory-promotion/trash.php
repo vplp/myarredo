d99add7a5599a37ac4f53d32ac15c04b3497a868
andrii bondarchuk <zndron@gmail.com>
2018-09-28 15:05:26 +0300

    upd

--- backend/modules/catalog/views/factory-promotion/trash.php
+++ backend/modules/catalog/views/factory-promotion/trash.php
@@ -5,6 +5,13 @@ use backend\widgets\GridView\GridView;
 use thread\widgets\grid\{
     ActionDeleteColumn, ActionRestoreColumn
 };
+use backend\modules\catalog\models\{
+    Factory
+};
+//
+use thread\widgets\grid\{
+    GridViewFilter
+};
 
 /**
  * @var \backend\modules\catalog\models\FactoryPromotion $model
@@ -15,6 +22,7 @@ echo GridView::widget([
     'dataProvider' => $model->trash(Yii::$app->request->queryParams),
     'filterModel' => $filter,
     'columns' => [
+        'id',
         [
             'format' => 'raw',
             'attribute' => 'updated_at',
@@ -24,10 +32,18 @@ echo GridView::widget([
             }
         ],
         [
+            'attribute' => Yii::t('app', 'Factory'),
+            'value' => function ($model) {
+                /** @var $model \backend\modules\catalog\models\Sale */
+                return ($model['factory']) ? $model['factory']['title'] : '';
+            },
+            'filter' => GridViewFilter::selectOne($filter, 'factory_id', Factory::dropDownList()),
+        ],
+        [
             'format' => 'raw',
             'label' => Yii::t('app', 'Список товаров'),
             'value' => function ($model) {
-                /** @var \frontend\modules\catalog\models\FactoryPromotion $model */
+                /** @var \backend\modules\catalog\models\FactoryPromotion $model */
                 $result = [];
                 foreach ($model->products as $product) {
                     $result[] = $product->lang->title;
@@ -39,14 +55,40 @@ echo GridView::widget([
             'format' => 'raw',
             'label' => Yii::t('app', 'Кол-во городов'),
             'value' => function ($model) {
-                /** @var \frontend\modules\catalog\models\FactoryPromotion $model */
+                /** @var \backend\modules\catalog\models\FactoryPromotion $model */
                 return count($model->cities);
             },
         ],
         [
-            'attribute' => 'cost',
-            'value' => 'cost',
-            'label' => Yii::t('app', 'Бюджет'),
+            'attribute' => 'amount',
+            'value' => 'amount',
+        ],
+        [
+            'attribute' => 'payment_status',
+            'value' => function ($model) {
+                /** @var \backend\modules\catalog\models\FactoryPromotion $model */
+                return $model->getPaymentStatusTitle();
+            },
+            'filter' => GridViewFilter::selectOne(
+                $filter,
+                'payment_status',
+                $model::paymentStatusKeyRange()
+            ),
+        ],
+        [
+            'attribute' => 'status',
+            'value' => function ($model) {
+                /** @var \backend\modules\catalog\models\FactoryPromotion $model */
+                return $model->getStatusTitle();
+            },
+            'filter' => GridViewFilter::selectOne(
+                $filter,
+                'status',
+                [
+                    0 => Yii::t('app', 'Завершена'),
+                    1 => Yii::t('app', 'Активная')
+                ]
+            ),
         ],
         [
             'class' => ActionDeleteColumn::class,
