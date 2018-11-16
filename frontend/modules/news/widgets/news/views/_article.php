fed11ae3378cbe34ce8e00f7b0d6c4e95773ac90
andrii bondarchuk <zndron@gmail.com>
2018-11-16 14:14:47 +0200

    upd

--- frontend/modules/news/widgets/news/views/_article.php
+++ frontend/modules/news/widgets/news/views/_article.php
@@ -7,5 +7,8 @@ use yii\helpers\Html;
 <div class="newsbox-item">
     <div><?= Html::encode($article['lang']['title']) ?></div>
     <div><?= $article['lang']['description'] ?></div>
-    <!-- <div><?= Html::a(Yii::t('app', 'Подробнее'), $article->getUrl(), ['class' => '']) ?></div>   -->
+
+    <?php if ($article['lang']['content'] !== '') { ?>
+        <div><?= Html::a(Yii::t('app', 'Подробнее'), $article->getUrl(), ['class' => '']) ?></div>
+    <?php } ?>
 </div>
