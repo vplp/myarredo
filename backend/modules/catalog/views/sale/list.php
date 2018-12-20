369a463b1dcbcc3e453ea3d5203e2fdd6cb00bc3
andrii bondarchuk <zndron@gmail.com>
2018-12-20 13:55:36 +0200

    upd

--- backend/modules/catalog/views/sale/list.php
+++ backend/modules/catalog/views/sale/list.php
@@ -45,7 +45,7 @@ echo GridView::widget([
         ],
         [
             'value' => function ($model) {
-                return $model->user->profile->name_company;
+                return ($model->user) ? $model->user->profile->name_company : '';
             },
             'filter' => GridViewFilter::selectOne(
                 $filter,
