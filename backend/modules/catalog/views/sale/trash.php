6ee7fe8807567539080d0f0c2fd49ca638c2615d
andrii bondarchuk <zndron@gmail.com>
2019-01-28 12:44:56 +0200

    upd

--- backend/modules/catalog/views/sale/trash.php
+++ backend/modules/catalog/views/sale/trash.php
@@ -1,9 +1,19 @@
 <?php
 
 use backend\widgets\GridView\GridView;
-use thread\widgets\grid\{
-    ActionDeleteColumn, ActionRestoreColumn
+use backend\modules\catalog\models\{
+    Category, Factory
 };
+use backend\modules\location\models\{
+    City
+};
+use backend\modules\user\models\User;
+//
+use thread\widgets\grid\{
+    ActionDeleteColumn, ActionRestoreColumn, GridViewFilter
+};
+
+/** @var $model \backend\modules\catalog\models\Sale */
 
 echo GridView::widget([
     'dataProvider' => $model->trash(Yii::$app->request->queryParams),
@@ -14,6 +24,43 @@ echo GridView::widget([
             'attribute' => 'title',
             'value' => 'lang.title',
             'label' => Yii::t('app', 'Title'),
+        ],
+        [
+            'attribute' => 'category',
+            'value' => function ($model) {
+                $result = [];
+                foreach ($model->category as $category) {
+                    $result[] = ($category->lang) ? $category->lang->title : '(не задано)';
+                }
+                return implode(', ', $result);
+            },
+            'label' => Yii::t('app', 'Category'),
+            'filter' => GridViewFilter::selectOne($filter, 'category', Category::dropDownList()),
+        ],
+        [
+            'attribute' => Yii::t('app', 'Factory'),
+            'value' => function ($model) {
+                /** @var $model \backend\modules\catalog\models\Sale */
+                return ($model['factory']) ? $model['factory']['title'] : $model['factory_name'];
+            },
+            'filter' => GridViewFilter::selectOne($filter, 'factory_id', Factory::dropDownList()),
+        ],
+        [
+            'value' => function ($model) {
+                return ($model->user) ? $model->user->profile->name_company : '';
+            },
+            'filter' => GridViewFilter::selectOne(
+                $filter,
+                'user_id',
+                User::dropDownListPartner()
+            ),
+        ],
+        [
+            'value' => function ($model) {
+                return ($model->city) ? $model->city->lang->title : '-';
+            },
+            'label' => Yii::t('app', 'City'),
+            'filter' => GridViewFilter::selectOne($filter, 'city_id', City::dropDownList()),
         ],
         [
             'class' => ActionDeleteColumn::class,
