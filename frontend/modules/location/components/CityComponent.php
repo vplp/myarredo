15495ed2551936c4f45bab09e49d3da0b50afccb
andrii bondarchuk <zndron@gmail.com>
2019-02-19 18:02:43 +0200

    upd

--- frontend/modules/location/components/CityComponent.php
+++ frontend/modules/location/components/CityComponent.php
@@ -88,7 +88,14 @@ class CityComponent extends Component
      */
     public function getPhoneMask()
     {
-        if (Yii::$app->language == 'it-IT') {
+        if (!Yii::$app->getUser()->isGuest && Yii::$app->user->identity->group->role == 'factory') {
+            $mask = [
+                '+7 (999) 999-99-99',
+                '+39 (9999) 99999',
+                '+39 (9999) 999-999',
+                '+39 (9999) 999-9999'
+            ];
+        } elseif (Yii::$app->language == 'it-IT') {
             $mask = '+39 (99) 999-99-99';
         } elseif (in_array($this->domain, ['by'])) {
             $mask = '+375 (99) 999-99-99';
