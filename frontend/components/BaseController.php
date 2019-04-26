05ad8ee67cca4e732bc4f37072c8692ad133ff42
andrii bondarchuk <zndron@gmail.com>
2019-04-26 16:01:54 +0300

    upd

--- frontend/components/BaseController.php
+++ frontend/components/BaseController.php
@@ -128,15 +128,17 @@ abstract class BaseController extends Controller
         $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
         // проверяем язык
-        if (!in_array($lang, array_keys($sites))) {
-            $lang = 'ru';
-        }
+//        if (!in_array($lang, array_keys($sites))) {
+//            $lang = 'ru';
+//        }
 
         // перенаправление на субдомен
         if (!$session->has('BrowserLanguage') && in_array($lang, array_keys($sites))) {
             $session->set('BrowserLanguage', $lang);
             header('Location: ' . $sites[$lang], false, 301);
             exit();
+        } else {
+            $session->set('BrowserLanguage', $lang);
         }
     }
 }
