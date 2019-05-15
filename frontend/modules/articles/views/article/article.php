d2e35880f58af4629c8e135c8f87dac7d409870e
Vados <javadosservice@gmail.com>
2019-05-15 13:02:18 +0300

    pull new upd

--- frontend/modules/articles/views/article/article.php
+++ frontend/modules/articles/views/article/article.php
@@ -9,17 +9,20 @@ use frontend\modules\articles\models\Article;
 $this->title = $model['lang']['title'];
 $this->context->breadcrumbs[] = $this->title;
 ?>
-
-<div class="main-new">
-    <div class="new-description">
-        <div class="data-news"><?= $model->getPublishedTime() ?></div>
-        <div class="title-news"><?= $model['lang']['title'] ?></div>
-        <div class="short-new"><?= $model['lang']['description'] ?></div>
-    </div>
-    <div class="img-for-new">
-        <?= Html::img($model->getArticleImage()) ?>
+<div class="myarredo-blog-wrap">
+    <div class="myarredo-blog">
+        <div class="single-articlebox">
+            <div class="new-description">
+                <div class="data-news"><?= $model->getPublishedTime() ?></div>
+                <div class="title-news"><?= $model['lang']['title'] ?></div>
+                <div class="short-new"><?= $model['lang']['description'] ?></div>
+            </div>
+            <div class="img-for-new">
+                <?= Html::img($model->getArticleImage()) ?>
+            </div>
+        </div>
+        <article class="article text-new">
+            <div class="article-text"><?= $model['lang']['content'] ?></div>
+        </article>
     </div>
 </div>
-<article class="article text-new">
-    <div class="article-text"><?= $model['lang']['content'] ?></div>
-</article>
