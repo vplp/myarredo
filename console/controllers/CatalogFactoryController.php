205dcfa35824249a183d5513e36d542c6c5e5491
andrii bondarchuk <zndron@gmail.com>
2019-06-10 14:07:41 +0300

    https://vipdesign.worksection.com/project/142833/5292167/7685202/

--- console/controllers/CatalogFactoryController.php
+++ console/controllers/CatalogFactoryController.php
@@ -49,7 +49,7 @@ class CatalogFactoryController extends Controller
 
             if (!empty($model->file_link) &&
                 is_file($path . '/' . $model->file_link) &&
-                strlen($model->file_link) < 8) {
+                strlen($model->file_link) <= 8) {
                 /**
                  * thumb
                  */
