752d2151525f57f4772688e468bdf2d3c2d3b85c
andrii bondarchuk <zndron@gmail.com>
2019-07-02 14:53:16 +0300

    https://vipdesign.worksection.com/project/142833/7447887/

--- frontend/modules/catalog/widgets/sale/SaleOfferPriceForm.php
+++ frontend/modules/catalog/widgets/sale/SaleOfferPriceForm.php
@@ -65,7 +65,7 @@ class SaleOfferPriceForm extends Widget
                                 'modelSale' => $modelSale,
                             ]
                         )
-                        ->setTo([$modelSale->user['email'], 'info@myarredo.ru', 'zndron@gmail.com'])
+                        ->setTo([$modelSale->user['email'], 'info@myarredo.ru'])
                         ->setSubject('Пользователь предложил свою цену на товар')
                         ->send();
 
