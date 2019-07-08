44e1fc57f68a4a71f201a008b2d4ef7eb01c8fee
andrii bondarchuk <zndron@gmail.com>
2019-07-08 17:06:46 +0300

    upd

--- frontend/modules/catalog/controllers/CategoryController.php
+++ frontend/modules/catalog/controllers/CategoryController.php
@@ -208,38 +208,6 @@ class CategoryController extends BaseController
     {
         if (Yii::$app->request->isAjax) {
             Yii::$app->getResponse()->format = Response::FORMAT_JSON;
-            $keys = Yii::$app->catalogFilter->keys;
-
-            if (Yii::$app->getRequest()->post('filter_on_main_page')) {
-                $category = Yii::$app->getRequest()->post('category');
-                $types = Yii::$app->getRequest()->post('types');
-                $price = Yii::$app->getRequest()->post('price');
-
-                $params = Yii::$app->catalogFilter->params;
-
-                if ($category) {
-                    $params[$keys['category']] = $category;
-                }
-
-                if ($types) {
-                    $params[$keys['type']][] = $types;
-                }
-
-                if (!empty($price['from']) && !empty($price['to'])) {
-                    $params[$keys['price']] = $price;
-                } elseif (empty($price['from']) && !empty($price['to'])) {
-                    $price['from'] = number_format(1, 0, '.', '');
-                    $params[$keys['price']] = $price;
-                } elseif (!empty($price['from']) && empty($price['to'])) {
-                    $price['to'] = number_format(Product::findBase()->max('price_from'), 0, '.', '');
-                    $params[$keys['price']] = $price;
-                }
-
-                $link = Yii::$app->catalogFilter->createUrl($params, ['/catalog/category/list']);
-
-                Yii::$app->response->redirect($link, 301);
-                Yii::$app->end();
-            }
 
             $category = ArrayHelper::map(Category::findBase()->all(), 'alias', 'lang.title');
             $types = ArrayHelper::map(Types::getWithProduct([]), 'alias', 'lang.title');
--- frontend/modules/catalog/widgets/filter/ProductFilterOnMainPage.php
+++ frontend/modules/catalog/widgets/filter/ProductFilterOnMainPage.php
@@ -4,12 +4,9 @@ namespace frontend\modules\catalog\widgets\filter;
 
 use Yii;
 use yii\base\Widget;
-use yii\helpers\{
-    Url, ArrayHelper
-};
 //
 use frontend\modules\catalog\models\{
-    Category, Types, Product
+    Product
 };
 
 /**
@@ -25,16 +22,6 @@ class ProductFilterOnMainPage extends Widget
     public $view = 'product_filter_on_main_page';
 
     /**
-     * @var object
-     */
-    //public $category = [];
-
-    /**
-     * @var object
-     */
-    //public $types = [];
-
-    /**
      * @return string
      * @throws \Throwable
      * @throws \yii\base\ExitException
@@ -42,45 +29,39 @@ class ProductFilterOnMainPage extends Widget
      */
     public function run()
     {
-//        $keys = Yii::$app->catalogFilter->keys;
-//
-//        if (Yii::$app->getRequest()->post('filter_on_main_page')) {
-//            $category = Yii::$app->getRequest()->post('category');
-//            $types = Yii::$app->getRequest()->post('types');
-//            $price = Yii::$app->getRequest()->post('price');
-//
-//            $params = Yii::$app->catalogFilter->params;
-//
-//            if ($category) {
-//                $params[$keys['category']] = $category;
-//            }
-//
-//            if ($types) {
-//                $params[$keys['type']][] = $types;
-//            }
-//
-//            if (!empty($price['from']) && !empty($price['to'])) {
-//                $params[$keys['price']] = $price;
-//            } elseif (empty($price['from']) && !empty($price['to'])) {
-//                $price['from'] = number_format(1, 0, '.', '');
-//                $params[$keys['price']] = $price;
-//            } elseif (!empty($price['from']) && empty($price['to'])) {
-//                $price['to'] = number_format(Product::findBase()->max('price_from'), 0, '.', '');
-//                $params[$keys['price']] = $price;
-//            }
-//
-//            $link = Yii::$app->catalogFilter->createUrl($params, ['/catalog/category/list']);
-//
-//            Yii::$app->response->redirect($link, 301);
-//            Yii::$app->end();
-//        }
-//
-//        $category = ArrayHelper::map(Category::findBase()->all(), 'alias', 'lang.title');
-//        $types = ArrayHelper::map(Types::getWithProduct([]), 'alias', 'lang.title');
+        $keys = Yii::$app->catalogFilter->keys;
 
-        return $this->render($this->view, [
-//            'category' => $category,
-//            'types' => $types,
-        ]);
+        if (Yii::$app->getRequest()->post('filter_on_main_page')) {
+            $category = Yii::$app->getRequest()->post('category');
+            $types = Yii::$app->getRequest()->post('types');
+            $price = Yii::$app->getRequest()->post('price');
+
+            $params = Yii::$app->catalogFilter->params;
+
+            if ($category) {
+                $params[$keys['category']] = $category;
+            }
+
+            if ($types) {
+                $params[$keys['type']][] = $types;
+            }
+
+            if (!empty($price['from']) && !empty($price['to'])) {
+                $params[$keys['price']] = $price;
+            } elseif (empty($price['from']) && !empty($price['to'])) {
+                $price['from'] = number_format(1, 0, '.', '');
+                $params[$keys['price']] = $price;
+            } elseif (!empty($price['from']) && empty($price['to'])) {
+                $price['to'] = number_format(Product::findBase()->max('price_from'), 0, '.', '');
+                $params[$keys['price']] = $price;
+            }
+
+            $link = Yii::$app->catalogFilter->createUrl($params, ['/catalog/category/list']);
+
+            Yii::$app->response->redirect($link, 301);
+            Yii::$app->end();
+        }
+
+        return $this->render($this->view, []);
     }
 }
