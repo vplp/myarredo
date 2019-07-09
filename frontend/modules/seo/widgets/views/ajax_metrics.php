b2d3c2194626fed3319a13d500615bf21a7081ee
andrii bondarchuk <zndron@gmail.com>
2019-07-09 17:38:03 +0300

    https://vipdesign.worksection.com/project/142833/7388445/7800686/

--- frontend/modules/seo/widgets/views/ajax_metrics.php
+++ frontend/modules/seo/widgets/views/ajax_metrics.php
@@ -6,11 +6,7 @@ use yii\helpers\Url;
  * @var $this \yii\web\View
  */
 
-?>
 
-    <div class="novelties"></div>
-
-<?php
 $url = Url::to(['/seo/metrics/ajax-get-metrics']);
 $script = <<<JS
 $.post('$url', {_csrf: $('#token').val()}, function(data){
