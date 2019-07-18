9c987df61c7bb2cdf0e71fa4000f2341af41e398
andrii bondarchuk <zndron@gmail.com>
2019-07-18 11:23:52 +0300

    https://vipdesign.worksection.com/project/142833/7388445/7800686/

--- frontend/themes/myarredo/layouts/parts/_yandex_metrika.php
+++ frontend/themes/myarredo/layouts/parts/_yandex_metrika.php
@@ -1,73 +1,71 @@
-<?php
+<?php if (Yii::$app->city->domain == 'ru') { ?>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
 
-use yii\web\View;
-
-/** @var $this \yii\web\View */
-
-$this->registerJsFile(
-    'https://mc.yandex.ru/metrika/watch.js',
-    [
-        'position' => View::POS_END,
-        'async' => true,
-        'defer' => true,
-    ]
-);
-
-if (Yii::$app->city->domain == 'ru') {
-    $script = <<<JS
-try {
-    var yaCounter24814823 = new Ya.Metrika({
-        id:24814823,
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-    });
-} catch(e) { }
-JS;
-
-    $this->registerJs($script, View::POS_END);
-    ?>
+        ym(24814823, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true,
+            trackHash: true
+        });
+    </script>
     <noscript>
         <div><img src="https://mc.yandex.ru/watch/24814823" style="position:absolute; left:-9999px;" alt=""/></div>
     </noscript>
+    <!-- /Yandex.Metrika counter -->
+<?php } elseif (Yii::$app->city->domain == 'by') { ?>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
 
-<?php } elseif (Yii::$app->city->domain == 'by') {
-    $script = <<<JS
-try {
-    var yaCounter24880844 = new Ya.Metrika({
-        id:24880844,
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-    });
-} catch(e) { }
-JS;
-
-    $this->registerJs($script, View::POS_END);
-    ?>
+        ym(24880844, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
     <noscript>
         <div><img src="https://mc.yandex.ru/watch/24880844" style="position:absolute; left:-9999px;" alt=""/></div>
     </noscript>
+    <!-- /Yandex.Metrika counter -->
+<?php } elseif (Yii::$app->city->domain == 'ua') { ?>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
 
-<?php } elseif (Yii::$app->city->domain == 'ua') {
-    $script = <<<JS
-try {
-    var yaCounter39354035 = new Ya.Metrika({
-        id:39354035,
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-    });
-} catch(e) { }
-JS;
-
-    $this->registerJs($script, View::POS_END);
-    ?>
+        ym(39354035, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
     <noscript>
         <div><img src="https://mc.yandex.ru/watch/39354035" style="position:absolute; left:-9999px;" alt=""/></div>
     </noscript>
-
+    <!-- /Yandex.Metrika counter -->
 <?php } ?>
