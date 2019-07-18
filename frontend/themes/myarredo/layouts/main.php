b093aac0654cc34c866e0cd559e176adb894f78b
andrii bondarchuk <zndron@gmail.com>
2019-07-18 11:37:01 +0300

    https://vipdesign.worksection.com/project/142833/7388445/7800686/

--- frontend/themes/myarredo/layouts/main.php
+++ frontend/themes/myarredo/layouts/main.php
@@ -42,7 +42,7 @@ $this->beginPage();
 
     <?= $this->render('parts/_jivosite', []) ?>
 
-    <?= $this->render('parts/_yandex_metrika', []) ?>
+    <?= ''//$this->render('parts/_yandex_metrika', []) ?>
 
     <?= $this->render('parts/_google_metrika', []) ?>
 
