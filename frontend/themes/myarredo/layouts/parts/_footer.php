1ae1af8290530b99fab58b02c1a34314bf7cfcf9
andrii bondarchuk <zndron@gmail.com>
2019-07-18 12:29:03 +0300

    https://vipdesign.worksection.com/project/142833/7388445/7800686/

--- frontend/themes/myarredo/layouts/parts/_footer.php
+++ frontend/themes/myarredo/layouts/parts/_footer.php
@@ -120,17 +120,3 @@ $bundle = AppAsset::register($this);
     </div>
 
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog"></div>
-
-<?php
-/**
- * сервис заказа обратного звонка
- */
-if (Yii::$app->getUser()->isGuest && Yii::$app->city->domain == 'ru' &&
-    !in_array(Yii::$app->controller->id, ['sale'])
-) { ?>
-    <script type="text/javascript">
-        var __cs = __cs || [];
-        __cs.push(["setCsAccount", "KNKIlw22peShQSsYcaBCDumFwgrDNrWx"]);
-    </script>
-    <script type="text/javascript" async src="//app.comagic.ru/static/cs.min.js"></script>
-<?php } ?>
