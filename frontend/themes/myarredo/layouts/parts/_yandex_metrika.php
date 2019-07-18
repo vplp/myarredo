de141539cd0a7b7d42c11efdddd8a48c3fdd1cb0
andrii bondarchuk <zndron@gmail.com>
2019-07-18 12:01:17 +0300

    https://vipdesign.worksection.com/project/142833/7388445/7800686/

--- frontend/themes/myarredo/layouts/parts/_yandex_metrika.php
+++ frontend/themes/myarredo/layouts/parts/_yandex_metrika.php
@@ -51,45 +51,89 @@ JS;
     <noscript>
         <div><img src="https://mc.yandex.ru/watch/24814823" style="position:absolute; left:-9999px;" alt=""/></div>
     </noscript>
-<?php } elseif (Yii::$app->city->domain == 'by') { ?>
-    <script type="text/javascript">
-        (function (m, e, t, r, i, k, a) {
-            m[i] = m[i] || function () {
-                (m[i].a = m[i].a || []).push(arguments)
-            };
-            m[i].l = 1 * new Date();
-            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-        })
-        (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+<?php } elseif (Yii::$app->city->domain == 'by') {
+    $script = <<<JS
+(function () {
+    var d = document;
+    var w = window;
 
-        ym(24880844, "init", {
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true
-        });
-    </script>
+    function l() {
+        setTimeout(function () {
+            (function (m, e, t, r, i, k, a) {
+                m[i] = m[i] || function () {
+                    (m[i].a = m[i].a || []).push(arguments)
+                };
+                m[i].l = 1 * new Date();
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+            })
+            (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+    
+            ym(24880844, "init", {
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true
+            });           
+        }, 5000);
+    }
+
+    if (d.readyState == 'complete') {
+        l();
+    } else {
+        if (w.attachEvent) {
+            w.attachEvent('onload', l);
+        } else {
+            w.addEventListener('load', l, false);
+        }
+    }
+})();
+JS;
+
+    $this->registerJs($script, View::POS_END);
+    ?>
     <noscript>
         <div><img src="https://mc.yandex.ru/watch/24880844" style="position:absolute; left:-9999px;" alt=""/></div>
     </noscript>
-<?php } elseif (Yii::$app->city->domain == 'ua') { ?>
-    <script type="text/javascript">
-        (function (m, e, t, r, i, k, a) {
-            m[i] = m[i] || function () {
-                (m[i].a = m[i].a || []).push(arguments)
-            };
-            m[i].l = 1 * new Date();
-            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-        })
-        (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+<?php } elseif (Yii::$app->city->domain == 'ua') {
+    $script = <<<JS
+(function () {
+    var d = document;
+    var w = window;
 
-        ym(39354035, "init", {
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true
-        });
-    </script>
+    function l() {
+        setTimeout(function () {
+            (function (m, e, t, r, i, k, a) {
+                m[i] = m[i] || function () {
+                    (m[i].a = m[i].a || []).push(arguments)
+                };
+                m[i].l = 1 * new Date();
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+            })
+            (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+    
+            ym(39354035, "init", {
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true
+            });           
+        }, 5000);
+    }
+
+    if (d.readyState == 'complete') {
+        l();
+    } else {
+        if (w.attachEvent) {
+            w.attachEvent('onload', l);
+        } else {
+            w.addEventListener('load', l, false);
+        }
+    }
+})();
+JS;
+
+    $this->registerJs($script, View::POS_END);
+    ?>
     <noscript>
         <div><img src="https://mc.yandex.ru/watch/39354035" style="position:absolute; left:-9999px;" alt=""/></div>
     </noscript>
