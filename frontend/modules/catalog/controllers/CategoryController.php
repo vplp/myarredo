09823b6e08237d00824a897136932039502dac46
andrii bondarchuk <zndron@gmail.com>
2019-09-10 10:58:28 +0300

    https://vipdesign.worksection.com/project/142833/5292167/7975730/

--- frontend/modules/catalog/controllers/CategoryController.php
+++ frontend/modules/catalog/controllers/CategoryController.php
@@ -82,7 +82,7 @@ class CategoryController extends BaseController
             'max' => $max
         ];
 
-        $queryParams['defaultPageSize'] = 48;
+        $queryParams['defaultPageSize'] = 21;
         $models = $model->search(ArrayHelper::merge(Yii::$app->request->queryParams, $queryParams));
 
         Yii::$app->metatag->render();
