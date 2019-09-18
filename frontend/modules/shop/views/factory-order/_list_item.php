7c8666a6441eeb45121f4df943fa087ee93659bd
andrii bondarchuk <zndron@gmail.com>
2019-09-18 14:09:10 +0300

    https://vipdesign.worksection.com/project/142833/5273252/8008020/

--- frontend/modules/shop/views/factory-order/_list_item.php
+++ frontend/modules/shop/views/factory-order/_list_item.php
@@ -3,9 +3,10 @@
 use yii\helpers\{
     Html, Url
 };
+use frontend\modules\shop\models\Order;
 
 /* @var $this yii\web\View */
-/* @var $modelOrder \frontend\modules\shop\models\Order */
+/* @var $modelOrder Order */
 
 ?>
 
@@ -21,16 +22,18 @@ use yii\helpers\{
 
     </div>
     <div class="form-wrap">
-
         <div class="form-group">
             <div><?= Yii::t('app', 'Response time') ?>:</div>
-            <?php
-            foreach ($modelOrder->orderAnswers as $key => $answer) {
-                echo '<div><strong>' . $answer['user']['profile']['lang']['name_company'] . '</strong></div>' .
-                    '<div>' . ($key + 1) . ') ' . $answer->getAnswerTime() . '</div>';
-            }
-            ?>
-        </div>
+            <?php foreach ($modelOrder->orderAnswers as $key => $answer) {
+                echo '<div><strong>' . $answer['user']['profile']['lang']['name_company'] . '</strong></div>';
 
+                if (in_array(substr($modelOrder->lang, 0, 2), ['en', 'it'])) {
+                    echo '<div>' . $answer['user']['email'] . '</div>' .
+                        '<div>' . $answer['user']['profile']['phone'] . '</div>';
+                }
+
+                echo '<div>' . ($key + 1) . ') ' . $answer->getAnswerTime() . '</div>';
+            } ?>
+        </div>
     </div>
 </div>
