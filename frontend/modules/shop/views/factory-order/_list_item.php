aee376635db5d1da86e3618529f4ee0383a9767a
andrii bondarchuk <zndron@gmail.com>
2019-09-18 15:11:11 +0300

    https://vipdesign.worksection.com/project/142833/5292167/8007956/

--- frontend/modules/shop/views/factory-order/_list_item.php
+++ frontend/modules/shop/views/factory-order/_list_item.php
@@ -23,16 +23,17 @@ use frontend\modules\shop\models\Order;
     </div>
     <div class="form-wrap">
         <div class="form-group">
+
+            <?php if (in_array(substr($modelOrder->lang, 0, 2), ['en', 'it'])) { ?>
+                <div><strong><?= $modelOrder->customer->full_name ?></strong></div>
+                <div><?= $modelOrder->customer->phone ?></div>
+                <div><?= $modelOrder->customer->email ?></div><br>
+            <?php } ?>
+
             <?php if ($modelOrder->orderAnswers) { ?>
                 <div><?= Yii::t('app', 'Response time') ?>:</div>
                 <?php foreach ($modelOrder->orderAnswers as $key => $answer) {
                     echo '<div><strong>' . $answer['user']['profile']['lang']['name_company'] . '</strong></div>';
-
-                    if (in_array(substr($modelOrder->lang, 0, 2), ['en', 'it'])) {
-                        echo '<div>' . $answer['user']['email'] . '</div>' .
-                            '<div>' . $answer['user']['profile']['phone'] . '</div>';
-                    }
-
                     echo '<div>' . ($key + 1) . ') ' . $answer->getAnswerTime() . '</div>';
                 } ?>
             <?php } ?>
