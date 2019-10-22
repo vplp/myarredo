f60904f7eee76aa6c9e5bd8a5e8a089159c3fb0f
andrii bondarchuk <zndron@gmail.com>
2019-10-22 16:04:56 +0300

    upd

--- frontend/components/BaseController.php
+++ frontend/components/BaseController.php
@@ -77,7 +77,7 @@ abstract class BaseController extends Controller
          */
         $lang = substr(Yii::$app->language, 0, 2);
 
-        if (in_array(Yii::$app->city->domain, ['ua', 'by'])) {
+        if (in_array(Yii::$app->city->domain, ['ua', 'by', 'com'])) {
             $session->set('currency', 'EUR');
         } elseif (!$session->has('currency') && $lang == 'ru') {
             $session->set('currency', 'RUB');
