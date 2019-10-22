93b8d45c8081994ab71eb9690e34b16b5d147d86
andrii bondarchuk <zndron@gmail.com>
2019-10-22 17:53:46 +0300

    upd

--- frontend/themes/myarredo/layouts/parts/_google_metrika.php
+++ frontend/themes/myarredo/layouts/parts/_google_metrika.php
@@ -116,4 +116,40 @@ JS;
 JS;
 
     $this->registerJs($script, View::POS_END);
+} elseif (Yii::$app->city->domain == 'com') {
+    $script = <<<JS
+(function () {
+    var id = 'UA-54015829-2';
+    var d = document;
+    var w = window;
+
+    function l() {
+        setTimeout(function () {
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = 'https://www.googletagmanager.com/gtag/js?id=' + id;
+            var ss = document.getElementsByTagName('script')[0];
+            ss.parentNode.insertBefore(s, ss);
+            
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', id);
+        }, 5000);
+    }
+
+    if (d.readyState == 'complete') {
+        l();
+    } else {
+        if (w.attachEvent) {
+            w.attachEvent('onload', l);
+        } else {
+            w.addEventListener('load', l, false);
+        }
+    }
+})();
+JS;
+
+    $this->registerJs($script, View::POS_END);
 }
--- frontend/themes/myarredo/layouts/parts/_yandex_metrika.php
+++ frontend/themes/myarredo/layouts/parts/_yandex_metrika.php
@@ -138,4 +138,47 @@ JS;
     <noscript>
         <div><img src="https://mc.yandex.ru/watch/39354035" style="position:absolute; left:-9999px;" alt=""/></div>
     </noscript>
+<?php } elseif (Yii::$app->city->domain == 'com') {
+    $script = <<<JS
+(function () {
+    var d = document;
+    var w = window;
+
+    function l() {
+        setTimeout(function () {
+            (function (m, e, t, r, i, k, a) {
+                m[i] = m[i] || function () {
+                    (m[i].a = m[i].a || []).push(arguments)
+                };
+                m[i].l = 1 * new Date();
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+            })
+            (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+    
+            ym(54688942, "init", {
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true
+            });           
+        }, 5000);
+    }
+
+    if (d.readyState == 'complete') {
+        l();
+    } else {
+        if (w.attachEvent) {
+            w.attachEvent('onload', l);
+        } else {
+            w.addEventListener('load', l, false);
+        }
+    }
+})();
+JS;
+
+    $this->registerJs($script, View::POS_END);
+    ?>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/39354035" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript>
 <?php }
