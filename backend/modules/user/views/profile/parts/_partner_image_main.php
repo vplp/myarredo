17c1b12999690b33de256622f2b81986aa9b84ec
andrii bondarchuk <zndron@gmail.com>
2019-12-06 10:52:28 +0200

    upd

--- backend/modules/user/views/profile/parts/_partner_image_main.php
+++ backend/modules/user/views/profile/parts/_partner_image_main.php
@@ -9,5 +9,5 @@ use backend\modules\user\models\{
 
 echo $form
     ->field($model, 'image_link')
-    ->label('Главное фото')
+    ->label(Yii::t('app', 'Главное фото') . ' (1070х800 px)')
     ->imageOne($model->getImageLink('image_link'));
