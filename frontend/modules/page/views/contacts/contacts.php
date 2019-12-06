83dff5ab27e2b75065e9d3bd69a011f99d259ef3
Vados <javadosservice@gmail.com>
2019-12-06 17:39:01 +0200

    add layout for partners

--- frontend/modules/page/views/contacts/contacts.php
+++ frontend/modules/page/views/contacts/contacts.php
@@ -21,80 +21,92 @@ $bundle = AppAsset::register($this);
             <div class="container large-container">
                 <div class="col-md-12">
                     <?= Html::tag('h1', $this->context->title); ?>
-                    <div class="of-conts ">
+                    <div class="of-conts">
                         <?php
                         if ($partners[0]['profile']['partner_in_city']) {
                             $mainPartner = array_shift($partners);
                             ?>
                             <!-- main partner -->
                             <div class="one-cont double-cont">
-                                <div class="img-part">
-                                    <?= Html::img($bundle->baseUrl . '/img/cont1.svg') ?>
-                                </div>
-                                <div class="info-part infopart">
+                                <div class="top-mainpart">
                                     <div class="main-sticker">
                                         <?= Yii::t('app', 'Главный партнер') ?>
                                     </div>
-
                                     <?= Html::tag('h2', $mainPartner->profile->getNameCompany()); ?>
 
-                                    <?= Html::a(
-                                        $mainPartner->profile->website,
-                                        $mainPartner->profile->website,
-                                        ['target' => '_blank', 'rel' => 'nofollow']
-                                    ) ?>
-
-                                    <div class="ico">
-                                        <?= Html::img($bundle->baseUrl . '/img/phone.svg') ?>
-                                    </div>
-
-                                    <?= Html::a(
-                                        $mainPartner->profile->phone,
-                                        'tel:' . $mainPartner->profile->phone,
-                                        []
-                                    ) ?>
-
-                                    <div class="ico">
-                                        <?= Html::img($bundle->baseUrl . '/img/marker-map.png') ?>
-                                    </div>
-                                    <div class="adres">
-                                        <?= isset($mainPartner->profile->city)
-                                            ? $mainPartner->profile->city->lang->title . '<br>'
-                                            : '' ?>
-                                        <?= $mainPartner->profile->lang->address ?>
-
-                                        <?= ($mainPartner->profile->working_hours_start && $mainPartner->profile->working_hours_end)
-                                            ? '<br>' . $mainPartner->profile->working_hours_start . ' - ' . $mainPartner->profile->working_hours_end
-                                            : '' ?>
-                                    </div>
-
                                     <?php if ($mainPartner->profile->phone2) { ?>
-                                        <div class="ico">
-                                            <?= Html::img($bundle->baseUrl . '/img/phone.svg') ?>
+                                        <div class="partner-phonebox">
+                                            <span class="for-phone">Телефон:</span>
+                                            <?= Html::a(
+                                                $mainPartner->profile->phone2,
+                                                'tel:' . $mainPartner->profile->phone2,
+                                                []
+                                            ); ?>
                                         </div>
-
-                                        <?= Html::a(
-                                            $mainPartner->profile->phone2,
-                                            'tel:' . $mainPartner->profile->phone2,
-                                            []
-                                        ); ?>
-                                    <?php } ?>
-
-                                    <?php if ($mainPartner->profile->lang->address2) { ?>
-                                        <div class="ico">
-                                            <?= Html::img($bundle->baseUrl . '/img/marker-map.png') ?>
-                                        </div>
-                                        <div class="adres">
-                                            <?= isset($mainPartner->profile->city)
-                                                ? $mainPartner->profile->city->lang->title . '<br>'
-                                                : '' ?>
-                                            <?= $mainPartner->profile->lang->address2 ?>
-                                            <?= ($mainPartner->profile->working_hours_start2 && $mainPartner->profile->working_hours_end2)
-                                                ? '<br>' . $mainPartner->profile->working_hours_start2 . ' - ' . $mainPartner->profile->working_hours_end2
-                                                : '' ?>
-                                        </div>
+                                        
                                     <?php } ?>
                                 </div>
+
+                                <div class="adress-mainpart">
+                                    <div class="partner-adressbox">
+                                        <div class="adres">
+                                            <span class="for-adress">Адресс:</span>
+                                                <?= isset($mainPartner->profile->city)
+                                                    ? $mainPartner->profile->city->lang->title . '<br>'
+                                                    : '' ?>
+                                                <?= $mainPartner->profile->lang->address ?>
+                                        </div>
+                                        <div class="adres">
+                                            <?= ($mainPartner->profile->working_hours_start && $mainPartner->profile->working_hours_end)
+                                            ? '<span class="for-mode">Режим работы салона:</span>' . $mainPartner->profile->working_hours_start . ' - ' . $mainPartner->profile->working_hours_end
+                                            : '' ?>
+                                        </div>
+                                    </div>
+                                    <div class="partner-phonebox">
+                                        <span class="for-phone">Телефон:</span>
+                                        <?= Html::a(
+                                            $mainPartner->profile->phone,
+                                            'tel:' . $mainPartner->profile->phone,
+                                            []
+                                        ) ?>
+                                    </div>
+                                </div>
+
+                                <?php if ($mainPartner->profile->lang->address2) { ?>
+                                    <div class="adress-mainpart">
+                                        <div class="partner-adressbox">
+
+                                            <div class="adres">
+                                                <span class="for-adress">Адресс:</span>
+                                                <?= isset($mainPartner->profile->city)
+                                                    ? $mainPartner->profile->city->lang->title . '<br>'
+                                                    : '' ?>
+                                                <?= $mainPartner->profile->lang->address2 ?>
+
+                                            </div>
+
+                                            <div class="adres">
+                                                <?= ($mainPartner->profile->working_hours_start2 && $mainPartner->profile->working_hours_end2)
+                                                    ? '<span class="for-mode">Режим работы салона:</span>' . $mainPartner->profile->working_hours_start2 . ' - ' . $mainPartner->profile->working_hours_end2
+                                                    : '' ?>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                <?php } ?>
+                                
+
+                                <div class="bottom-mainpart">
+                                    <div class="partner-sitebox">
+                                        <span class="for-site">Адресс сайта:</span>
+                                        <?= Html::a(
+                                            $mainPartner->profile->website,
+                                            $mainPartner->profile->website,
+                                            ['target' => '_blank', 'rel' => 'nofollow']
+                                        ) ?>
+                                    </div>
+                                </div>
+
                                 <div class="partner-formbox hidden">
                                     <?php /*echo FormFeedback::widget([
                                         'partner_id' => $mainPartner->id,
