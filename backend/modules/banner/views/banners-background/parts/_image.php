19a5c5b5dd51918ca1a270bc7aa3ea367d26f501
andrii bondarchuk <zndron@gmail.com>
2019-12-23 14:33:58 +0200

    upd

--- backend/modules/banner/views/banners-background/parts/_image.php
+++ backend/modules/banner/views/banners-background/parts/_image.php
@@ -11,4 +11,7 @@ use backend\modules\banner\models\{
  * @var $form ActiveForm
  */
 
-echo $form->field($model, 'image_link')->imageOne($model->getImageLink());
+echo $form
+    ->field($model, 'image_link')
+    ->imageOne($model->getImageLink())
+    ->label(Yii::t('app', 'Image link') . ' 960x1200px');
