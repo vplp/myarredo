26f5ffec0a49dc0bc33c726aaa3e0f5b0cb4d581
andrii bondarchuk <zndron@gmail.com>
2020-02-03 09:50:21 +0200

    upd

--- frontend/modules/catalog/views/product/view.php
+++ frontend/modules/catalog/views/product/view.php
@@ -65,6 +65,14 @@ $this->title = $this->context->title;
                                 <div class="price-availability" itemprop="offers" itemscope
                                      itemtype="http://schema.org/Offer">
 
+                                    <?php if (!Yii::$app->getUser()->isGuest && in_array(Yii::$app->user->identity->group->role, ['admin', 'partner'])) { ?>
+                                        <div class="price-sticker">
+                                            <?= Yii::t('app', 'Цена от') ?><span>&#126;</span>
+                                            <span><?= Yii::$app->currency->getValue($model['price_from'], $model['currency']); ?>
+                                            &nbsp;<span class="currency"><?= Yii::$app->currency->symbol ?></span></span>
+                                        </div>
+                                    <?php } ?>
+
                                     <?php /*if (Yii::$app->city->isShowPrice() && !$model['removed'] && $model['price_from'] > 0) { ?>
                                     <div class="price-sticker">
                                         <?= Yii::t('app', 'Цена от') ?><span>&#126;</span>
