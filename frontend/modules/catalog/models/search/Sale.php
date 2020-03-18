bdc50219987e2588d6bf570943190c8070f517b3
andrii bondarchuk <zndron@gmail.com>
2020-03-18 16:01:34 +0200

    upd

--- frontend/modules/catalog/models/search/Sale.php
+++ frontend/modules/catalog/models/search/Sale.php
@@ -153,7 +153,7 @@ class Sale extends SaleModel implements BaseBackendSearchModel
         /** orderBy */
 
         if (in_array(Yii::$app->city->getCityId(), [1, 2, 4, 159])) {
-            $order[] = self::tableName() . '.is_composition DESC';
+            //$order[] = self::tableName() . '.is_composition DESC';
         }
         $order[] = self::tableName() . '.updated_at DESC';
         $query->orderBy(implode(',', $order));
