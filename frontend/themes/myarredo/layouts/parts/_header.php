a482ee8c109b4325be38ad16effa8ff56b6c57db
Javados <javadosservice@gmail.com>
2020-06-24 20:29:51 +0300

    https://vipdesign.worksection.com/project/142833/8539933/8540029/

--- frontend/themes/myarredo/layouts/parts/_header.php
+++ frontend/themes/myarredo/layouts/parts/_header.php
@@ -4,10 +4,10 @@ use yii\helpers\{
     Html, Url
 };
 use yii\bootstrap\ActiveForm;
-//
+
 use frontend\modules\sys\widgets\lang\LangSwitch;
 use frontend\modules\catalog\widgets\menu\CatalogMenu;
-use frontend\modules\catalog\widgets\menu_mobile\CatalogMenuMobile;
+// use frontend\modules\catalog\widgets\menu_mobile\CatalogMenuMobile;
 use frontend\modules\shop\widgets\cart\Cart;
 use frontend\modules\location\widgets\{
     ChangeCity, ChangeCurrency
@@ -527,7 +527,8 @@ use frontend\modules\user\widgets\menu\UserMenu;
             echo UserMenu::widget(['view' => 'user_menu_mobile']);
         } else {
             echo UserMenu::widget(['view' => 'user_menu_mobile']);
-            echo CatalogMenuMobile::widget([]);
+            // echo CatalogMenuMobile::widget([]);
+            echo CatalogMenu::widget([]);
         } ?>
 
         <div class="bot-list">
