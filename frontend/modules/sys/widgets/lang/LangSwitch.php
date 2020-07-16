cc428046307421a6d566e6f5e986955f386f5b72
andrii bondarchuk <zndron@gmail.com>
2020-07-16 17:27:26 +0300

    upd

--- frontend/modules/sys/widgets/lang/LangSwitch.php
+++ frontend/modules/sys/widgets/lang/LangSwitch.php
@@ -68,7 +68,7 @@ class LangSwitch extends Widget
             /**
              * $url
              */
-            if (!in_array($lang['alias'], ['it', 'en', 'de']) && !in_array(Yii::$app->city->domain, ['com', 'de'])) {
+            if (in_array($lang['alias'], ['it', 'en', 'de']) && in_array(Yii::$app->city->domain, ['com', 'de'])) {
                 $url = 'https://www.myarredo.ru';
             } elseif (in_array($lang['alias'], ['de'])) {
                 $url = 'https://www.myarredo.de';
