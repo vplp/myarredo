44af7d277af18d18325734c89d599fd14ad23f27
andrii bondarchuk <zndron@gmail.com>
2020-07-28 14:35:31 +0300

    https://vipdesign.worksection.com/project/142833/5292167/8618341/

--- backend/modules/catalog/views/product/parts/_settings.php
+++ backend/modules/catalog/views/product/parts/_settings.php
@@ -16,7 +16,6 @@ use backend\modules\catalog\models\{
 
 ?>
 
-
 <?= $form->text_line($model, 'alias') ?>
 
 <?= $form->text_line($model, 'alias_en') ?>
@@ -24,7 +23,6 @@ use backend\modules\catalog\models\{
 <?= $form->text_line($model, 'alias_it') ?>
 
 <?= $form->text_line($model, 'alias_de') ?>
-
 
 <?= $form->text_line($model, 'article') ?>
 
