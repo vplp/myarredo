f9429af2d0bb159ca9f5e8de5b0a08d07f24e689
andrii bondarchuk <zndron@gmail.com>
2020-11-12 10:29:29 +0200

    upd

---
+++ frontend/modules/api/ApiModule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace frontend\modules\api;
+
+use yii\base\Module;
+
+/**
+ * Class TurboShopModule
+ *
+ * @package frontend\modules\payment
+ */
+class TurboShopModule extends Module
+{
+
+}
--- frontend/themes/myarredo/layouts/main.php
+++ frontend/themes/myarredo/layouts/main.php
@@ -24,6 +24,7 @@ $this->beginPage();
     <meta name="robots" content="noyaca"/>
 
     <link rel="icon" href="https://img.<?= DOMAIN_NAME . '.' . DOMAIN_TYPE ?>/myarredo-ico.svg" type="image/svg+xml"/>
+    <link rel="icon" href="https://img.<?= DOMAIN_NAME . '.' . DOMAIN_TYPE ?>/favicon.ico" type="image/x-icon"/>
 
     <!-- preloader styles -->
     <style>
