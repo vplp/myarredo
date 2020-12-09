2cf39729d177941d028d174b9804d452bc033d74
andrii bondarchuk <zndron@gmail.com>
2020-12-09 09:19:27 +0200

    upd

--- frontend/modules/catalog/models/Product.php
+++ frontend/modules/catalog/models/Product.php
@@ -411,7 +411,7 @@ class Product extends \common\modules\catalog\models\Product
             $alt[] = $this->article;
         }
 
-        if ($this->collections_id) {
+        if ($this->collection) {
             $alt[] = $this->collection->title;
         }
 
