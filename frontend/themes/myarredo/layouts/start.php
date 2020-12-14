3200ad723cc728186b48c654f7b3c1d611bdad32
andrii bondarchuk <zndron@gmail.com>
2020-12-14 12:17:17 +0200

    upd

--- frontend/themes/myarredo/layouts/start.php
+++ frontend/themes/myarredo/layouts/start.php
@@ -26,10 +26,7 @@ $bundle = AppAsset::register($this);
         <!-- container-wrap -->
         <div class="container-wrap">
 
-            <?php if ($this->beginCache('BannerList' . DOMAIN_TYPE, ['duration' => 60 * 15])) {
-                echo BannerList::widget(['type' => 'main', 'city_id' => Yii::$app->city->getCityId()]);
-                $this->endCache();
-            } ?>
+            <?= BannerList::widget(['type' => 'main', 'city_id' => Yii::$app->city->getCityId()]); ?>
 
             <!-- best-price -->
             <div class="best-price">
