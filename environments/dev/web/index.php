d5b1ae988d26d4d5e946a4ab3aa3456976f6f202
andrii bondarchuk <zndron@gmail.com>
2020-12-24 15:05:46 +0200

    upd

--- environments/dev/web/index.php
+++ environments/dev/web/index.php
@@ -3,7 +3,12 @@
 $exp = explode('.', $_SERVER['HTTP_HOST']);
 
 defined('DOMAIN_NAME') or define('DOMAIN_NAME', $exp[1]);
-defined('DOMAIN_TYPE') or define('DOMAIN_TYPE', $exp[2] != 'test' ? $exp[2] : 'ru');
+
+if ($exp[2] && $exp[3]) {
+    defined('DOMAIN_TYPE') or define('DOMAIN_TYPE', $exp[2] . '.' . $exp[3]);
+} else {
+    defined('DOMAIN_TYPE') or define('DOMAIN_TYPE', $exp[2] != 'test' ? $exp[2] : 'ru');
+}
 
 $baseDirExecPath = dirname(__DIR__, 1);
 
--- frontend/components/BaseController.php
+++ frontend/components/BaseController.php
@@ -59,8 +59,8 @@ abstract class BaseController extends Controller
 
         $lang = substr(Yii::$app->language, 0, 2);
 
-        // de domain
-        if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) && DOMAIN_TYPE == 'de' && !in_array($lang, ['il'])) {
+        // il domain
+        if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) && DOMAIN_TYPE == 'co.il' && !in_array($lang, ['il'])) {
             Yii::$app->response->redirect('https://' . 'www.myarredo.co.il/', 301);
             yii::$app->end();
         } elseif (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) && in_array(DOMAIN_TYPE, ['ru', 'ua', 'by', 'com', 'de']) && in_array($lang, ['il'])) {
