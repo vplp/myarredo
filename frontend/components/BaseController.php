965de0cc5e858f9f3a95b3610ef4cd89a3b6fea3
andrii bondarchuk <zndron@gmail.com>
2020-12-24 15:55:20 +0200

    upd

--- frontend/components/BaseController.php
+++ frontend/components/BaseController.php
@@ -92,11 +92,11 @@ abstract class BaseController extends Controller
             yii::$app->end();
         }
 
-        if (preg_match('!/{2,}!', $_SERVER['REQUEST_URI'])) {
-            $url = preg_replace('!/{2,}!', '/', $_SERVER['REQUEST_URI']);
-            header('Location: ' . $url, false, 301);
-            exit();
-        }
+//        if (preg_match('!/{2,}!', $_SERVER['REQUEST_URI'])) {
+//            $url = preg_replace('!/{2,}!', '/', $_SERVER['REQUEST_URI']);
+//            header('Location: ' . $url, false, 301);
+//            exit();
+//        }
 
         $this->getAlternateHreflang();
 
