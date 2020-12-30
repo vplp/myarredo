d8cc6ee2d1b8d829d2887de1bfa5c5a5b023cbad
javados <javadosservice@gmail.com>
2020-12-30 15:58:27 +0200

    замена шрифтов для задачи - https://vipdesign.worksection.com/project/142833/7388445/8879977/

--- frontend/themes/myarredo/layouts/main.php
+++ frontend/themes/myarredo/layouts/main.php
@@ -245,6 +245,9 @@ $this->beginPage();
         }
     </style>
     <!-- end preloader styles -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Vollkorn:ital,wght@0,400;0,500;0,700;0,800;1,400&display=swap" rel="stylesheet">
 
     <?php $this->head(); ?>
     <!--[if lt IE 9]>
