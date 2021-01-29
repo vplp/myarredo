ee7cd4a7f896d967ce52bb19de7d2780c98d6cef
andrii bondarchuk <zndron@gmail.com>
2021-01-29 17:52:47 +0200

    upd

--- frontend/modules/catalog/views/product/parts/_factory_files.php
+++ frontend/modules/catalog/views/product/parts/_factory_files.php
@@ -15,53 +15,55 @@ use frontend\modules\catalog\models\{
 
 ?>
 
-<?php if (!Yii::$app->getUser()->isGuest && Yii::$app->user->identity->profile->isPdfAccess()) { ?>
-    <div class="downloads">
-
+<?php //if (!Yii::$app->getUser()->isGuest && Yii::$app->user->identity->profile->isPdfAccess()) { ?>
+<div class="downloads">
+    <p class="inpdf-title"><?= Yii::t('app', 'Посмотреть каталоги') ?></p>
+    <ul class="inpdf-list">
         <?php if (!empty($model->factoryCatalogsFiles)) { ?>
-            <p class="inpdf-title"><?= Yii::t('app', 'Посмотреть каталоги') ?></p>
-            <ul class="inpdf-list">
+            <?php foreach ($model->factoryCatalogsFiles as $catalogFile) {
+                if ($fileLink = $catalogFile->getFileLink()) { ?>
+                    <li>
+                        <?= Html::a(
+                            $catalogFile->title . ' <i class="fa fa-file-pdf-o" aria-hidden="true"></i>',
+                            Url::toRoute(['/catalog/factory/pdf-viewer']) . '?file=' . $fileLink . '&search=' . $model->article,
+                            [
+                                'target' => '_blank',
+                                'class' => 'click-on-factory-file btn-inpdf',
+                                'data-id' => $catalogFile->id
+                            ]
+                        ) ?>
+                    </li>
+                <?php }
+            } ?>
+        <?php } else { ?>
+            <li>
 
-                <?php foreach ($model->factoryCatalogsFiles as $catalogFile) {
-                    if ($fileLink = $catalogFile->getFileLink()) { ?>
-                        <li>
-                            <?= Html::a(
-                                $catalogFile->title . ' <i class="fa fa-file-pdf-o" aria-hidden="true"></i>',
-                                Url::toRoute(['/catalog/factory/pdf-viewer']) . '?file=' . $fileLink . '&search=' . $model->article,
-                                [
-                                    'target' => '_blank',
-                                    'class' => 'click-on-factory-file btn-inpdf',
-                                    'data-id' => $catalogFile->id
-                                ]
-                            ) ?>
-                        </li>
-                    <?php }
-                } ?>
-            </ul>
+            </li>
         <?php } ?>
+    </ul>
 
-        <?php if (!empty($model->factoryPricesFiles)) { ?>
-            <p class="inpdf-title"><?= Yii::t('app', 'Посмотреть прайс листы') ?></p>
-            <ul class="inpdf-list">
+    <?php if (!empty($model->factoryPricesFiles)) { ?>
+        <p class="inpdf-title"><?= Yii::t('app', 'Посмотреть прайс листы') ?></p>
+        <ul class="inpdf-list">
 
-                <?php foreach ($model->factoryPricesFiles as $priceFile) {
-                    if ($fileLink = $priceFile->getFileLink()) { ?>
-                        <li>
-                            <?= Html::a(
-                                $priceFile->title . ' <i class="fa fa-file-pdf-o" aria-hidden="true"></i>',
-                                Url::toRoute(['/catalog/factory/pdf-viewer']) . '?file=' . $fileLink . '&search=' . $model->article,
-                                [
-                                    'target' => '_blank',
-                                    'class' => 'click-on-factory-file btn-inpdf',
-                                    'data-id' => $priceFile->id
-                                ]
-                            ) ?>
-                        </li>
-                    <?php }
-                } ?>
+            <?php foreach ($model->factoryPricesFiles as $priceFile) {
+                if ($fileLink = $priceFile->getFileLink()) { ?>
+                    <li>
+                        <?= Html::a(
+                            $priceFile->title . ' <i class="fa fa-file-pdf-o" aria-hidden="true"></i>',
+                            Url::toRoute(['/catalog/factory/pdf-viewer']) . '?file=' . $fileLink . '&search=' . $model->article,
+                            [
+                                'target' => '_blank',
+                                'class' => 'click-on-factory-file btn-inpdf',
+                                'data-id' => $priceFile->id
+                            ]
+                        ) ?>
+                    </li>
+                <?php }
+            } ?>
 
-            </ul>
-        <?php } ?>
+        </ul>
+    <?php } ?>
 
-    </div>
-<?php } ?>
+</div>
+<?php //} ?>
