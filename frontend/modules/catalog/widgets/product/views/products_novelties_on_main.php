d9beb3ce5fea0e69e3d0a40eb756db4d155f77e2
javados <javadosservice@gmail.com>
2021-02-11 14:30:39 +0200

    https://vipdesign.worksection.com/project/142833/5292167/8958553/

--- frontend/modules/catalog/widgets/product/views/products_novelties_on_main.php
+++ frontend/modules/catalog/widgets/product/views/products_novelties_on_main.php
@@ -96,10 +96,10 @@ $detect = new Mobile_Detect();
 
                     <div class="arr-cont">
                         <a class="left left-arr" href="#novelties-slider" role="button" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left"></span>
+                            <i class="fa fa-chevron-left" aria-hidden="true"></i>
                         </a>
                         <a class="right right-arr" href="#novelties-slider" role="button" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <i class="fa fa-chevron-right" aria-hidden="true"></i>
                         </a>
                     </div>
 
