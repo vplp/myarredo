731719a81b84b2759eb7018c2bb3af028c70dc1a
andrii bondarchuk <zndron@gmail.com>
2021-03-02 17:12:51 +0200

    upd

--- frontend/modules/catalog/views/factory/pdfjs.php
+++ frontend/modules/catalog/views/factory/pdfjs.php
@@ -3,9 +3,11 @@
 $this->title = $this->context->title;
 
 $script = <<<JS
-setTimeout(function () {
-    document.title = '$this->title';
-}, 1500);
+window.onload = function() {
+    setTimeout(function () {
+        document.title = '$this->title';
+    }, 1500);
+}
 JS;
 
 $this->registerJs($script);
