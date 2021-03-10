f15fa25b4d78d1f5905df3b4e800c9cd3400bc05
andrii bondarchuk <zndron@gmail.com>
2021-03-10 17:29:09 +0200

    upd

--- frontend/modules/catalog/models/ElasticSearchSale.php
+++ frontend/modules/catalog/models/ElasticSearchSale.php
@@ -219,28 +219,7 @@ class ElasticSearchSale extends ActiveRecord
     {
         $lang = substr(Yii::$app->language, 0, 2);
 
-        $query = self::find()->with([
-            'product',
-            'product.factory',
-            //'product.country',
-            //'product.city'
-        ]);
-
-//        $params['country'] = Yii::$app->city->getCountryId();
-//
-//        if (isset($params['country'])) {
-//            $query->andFilterWhere([self::index() . '.country_id' => $params['country']]);
-//        }
-
-//        $params['city'] = Yii::$app->city->getCityId();
-//
-//        if (isset($params['country'])) {
-//            $query->andWhere([City::tableName() . '.id' => $params['country']]);
-//        }
-//
-//        if (isset($params['city'])) {
-//            $query->andWhere([City::tableName() . '.id' => $params['city']]);
-//        }
+        $query = self::find()->with(['product', 'product.factory']);
 
         /** @var Catalog $module */
         $module = Yii::$app->getModule('catalog');
