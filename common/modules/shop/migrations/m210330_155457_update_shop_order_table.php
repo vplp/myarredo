9ab3042d4f10cf778664e7163c3afdc03647cdc1
andrii bondarchuk <zndron@gmail.com>
2021-03-31 14:49:57 +0300

    https://vipdesign.worksection.com/project/142833/5273252/8937133/

--- common/modules/shop/migrations/m210330_155457_update_shop_order_table.php
+++ common/modules/shop/migrations/m210330_155457_update_shop_order_table.php
@@ -25,42 +25,42 @@ class m210330_155457_update_shop_order_table extends Migration
         $this->addColumn($this->table, 'send_answer', "enum('0','1') NOT NULL DEFAULT '0'");
         $this->createIndex('send_answer', $this->table, 'send_answer');
 
-        $rows = (new \yii\db\Query())
-            ->from($this->table)
-            ->all();
-
-        foreach ($rows as $row) {
-            $date1 = new \DateTime();
-            $date2 = new \DateTime(date(DATE_ATOM, $row['created_at']));
-
-            $diff = $date2->diff($date1);
-            $hours = $diff->h;
-            $hours = $hours + ($diff->days * 24);
-
-            $countOrderAnswers = (new \yii\db\Query())
-                ->from($this->tableOrderAnswers)
-                ->where(['order_id' => $row['id']])
-                ->count();
-
-            $send_answer = '0';
-
-            if ($hours >= 3/* && $countOrderAnswers >= 1*/) {
-                $send_answer = '1';
-            } elseif ($countOrderAnswers >= 3) {
-                $send_answer = '1';
-            }
-
-            $connection = Yii::$app->db;
-            $connection->createCommand()
-                ->update(
-                    $this->table,
-                    [
-                        'send_answer' => $send_answer
-                    ],
-                    'id = ' . $row['id']
-                )
-                ->execute();
-        }
+//        $rows = (new \yii\db\Query())
+//            ->from($this->table)
+//            ->all();
+//
+//        foreach ($rows as $row) {
+//            $date1 = new \DateTime();
+//            $date2 = new \DateTime(date(DATE_ATOM, $row['created_at']));
+//
+//            $diff = $date2->diff($date1);
+//            $hours = $diff->h;
+//            $hours = $hours + ($diff->days * 24);
+//
+//            $countOrderAnswers = (new \yii\db\Query())
+//                ->from($this->tableOrderAnswers)
+//                ->where(['order_id' => $row['id']])
+//                ->count();
+//
+//            $send_answer = '0';
+//
+//            if ($hours >= 3/* && $countOrderAnswers >= 1*/) {
+//                $send_answer = '1';
+//            } elseif ($countOrderAnswers >= 3) {
+//                $send_answer = '1';
+//            }
+//
+//            $connection = Yii::$app->db;
+//            $connection->createCommand()
+//                ->update(
+//                    $this->table,
+//                    [
+//                        'send_answer' => $send_answer
+//                    ],
+//                    'id = ' . $row['id']
+//                )
+//                ->execute();
+//        }
     }
 
     public function safeDown()
