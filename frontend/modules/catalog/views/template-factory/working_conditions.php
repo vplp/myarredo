67e7b9e20098bfe254b62eeb7143f94276ef4cd6
andrii bondarchuk <zndron@gmail.com>
2021-08-18 15:43:36 +0300

    upd

--- frontend/modules/catalog/views/template-factory/working_conditions.php
+++ frontend/modules/catalog/views/template-factory/working_conditions.php
@@ -6,7 +6,7 @@ use frontend\modules\catalog\models\{
 };
 
 /**
- * @var $model Factory
+ * @var $factory Factory
  */
 
 $this->title = $this->context->title;
@@ -20,30 +20,30 @@ $this->title = $this->context->title;
 
                 <?= Html::tag('h1', $this->context->title); ?>
 
-                <b><?= $model->getAttributeLabel('factory_discount') ?></b>: <?= $model->factory_discount ?><br>
-                <b><?= $model->getAttributeLabel('factory_discount_with_exposure') ?></b>: <?= $model->factory_discount_with_exposure ?>
+                <b><?= $factory->getAttributeLabel('factory_discount') ?></b>: <?= $factory->factory_discount ?><br>
+                <b><?= $factory->getAttributeLabel('factory_discount_with_exposure') ?></b>: <?= $factory->factory_discount_with_exposure ?>
                 <br>
-                <b><?= $model->getAttributeLabel('factory_discount_on_exposure') ?></b>: <?= $model->factory_discount_on_exposure ?>
+                <b><?= $factory->getAttributeLabel('factory_discount_on_exposure') ?></b>: <?= $factory->factory_discount_on_exposure ?>
                 <br>
 
-                <b><?= $model->lang->getAttributeLabel('wc_expiration_date') ?></b>: <?= $model->lang->wc_expiration_date ?>
+                <b><?= $factory->lang->getAttributeLabel('wc_expiration_date') ?></b>: <?= $factory->lang->wc_expiration_date ?>
                 <br>
-                <b><?= $model->lang->getAttributeLabel('wc_terms_of_payment') ?></b>: <?= $model->lang->wc_terms_of_payment ?>
+                <b><?= $factory->lang->getAttributeLabel('wc_terms_of_payment') ?></b>: <?= $factory->lang->wc_terms_of_payment ?>
                 <br>
 
                 <?php if (in_array(DOMAIN_TYPE, ['ru', 'ua', 'by'])) { ?>
-                    <b><?= $model->lang->getAttributeLabel('wc_phone_supplier') ?></b>: <?= $model->lang->wc_phone_supplier ?>
+                    <b><?= $factory->lang->getAttributeLabel('wc_phone_supplier') ?></b>: <?= $factory->lang->wc_phone_supplier ?>
                     <br>
-                    <b><?= $model->lang->getAttributeLabel('wc_email_supplier') ?></b>: <?= $model->lang->wc_email_supplier ?>
+                    <b><?= $factory->lang->getAttributeLabel('wc_email_supplier') ?></b>: <?= $factory->lang->wc_email_supplier ?>
                     <br>
-                    <b><?= $model->lang->getAttributeLabel('wc_contact_person_supplier') ?></b>: <?= $model->lang->wc_contact_person_supplier ?>
+                    <b><?= $factory->lang->getAttributeLabel('wc_contact_person_supplier') ?></b>: <?= $factory->lang->wc_contact_person_supplier ?>
                     <br>
                 <?php } else { ?>
-                    <b><?= $model->lang->getAttributeLabel('wc_phone_factory') ?></b>: <?= $model->lang->wc_phone_factory ?>
+                    <b><?= $factory->lang->getAttributeLabel('wc_phone_factory') ?></b>: <?= $factory->lang->wc_phone_factory ?>
                     <br>
-                    <b><?= $model->lang->getAttributeLabel('wc_email_factory') ?></b>: <?= $model->lang->wc_email_factory ?>
+                    <b><?= $factory->lang->getAttributeLabel('wc_email_factory') ?></b>: <?= $factory->lang->wc_email_factory ?>
                     <br>
-                    <b><?= $model->lang->getAttributeLabel('wc_contact_person_factory') ?></b>: <?= $model->lang->wc_contact_person_factory ?>
+                    <b><?= $factory->lang->getAttributeLabel('wc_contact_person_factory') ?></b>: <?= $factory->lang->wc_contact_person_factory ?>
                     <br>
                 <?php } ?>
 
