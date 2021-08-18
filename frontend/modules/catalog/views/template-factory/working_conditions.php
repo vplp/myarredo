3b2aa667c8b5f5958919c20f55ff5fd0a175b738
andrii bondarchuk <zndron@gmail.com>
2021-08-18 15:50:49 +0300

    upd

--- frontend/modules/catalog/views/template-factory/working_conditions.php
+++ frontend/modules/catalog/views/template-factory/working_conditions.php
@@ -26,27 +26,28 @@ $this->title = $this->context->title;
                 <b><?= $factory->getAttributeLabel('factory_discount_on_exposure') ?></b>: <?= $factory->factory_discount_on_exposure ?>
                 <br>
 
-                <b><?= $factory->lang->getAttributeLabel('wc_expiration_date') ?></b>: <?= $factory->lang->wc_expiration_date ?>
-                <br>
-                <b><?= $factory->lang->getAttributeLabel('wc_terms_of_payment') ?></b>: <?= $factory->lang->wc_terms_of_payment ?>
-                <br>
+                <?php if ($factory->lang) { ?>
+                    <b><?= $factory->lang->getAttributeLabel('wc_expiration_date') ?></b>: <?= $factory->lang->wc_expiration_date ?>
+                    <br>
+                    <b><?= $factory->lang->getAttributeLabel('wc_terms_of_payment') ?></b>: <?= $factory->lang->wc_terms_of_payment ?>
+                    <br>
 
-                <?php if (in_array(DOMAIN_TYPE, ['ru', 'ua', 'by'])) { ?>
-                    <b><?= $factory->lang->getAttributeLabel('wc_phone_supplier') ?></b>: <?= $factory->lang->wc_phone_supplier ?>
-                    <br>
-                    <b><?= $factory->lang->getAttributeLabel('wc_email_supplier') ?></b>: <?= $factory->lang->wc_email_supplier ?>
-                    <br>
-                    <b><?= $factory->lang->getAttributeLabel('wc_contact_person_supplier') ?></b>: <?= $factory->lang->wc_contact_person_supplier ?>
-                    <br>
-                <?php } else { ?>
-                    <b><?= $factory->lang->getAttributeLabel('wc_phone_factory') ?></b>: <?= $factory->lang->wc_phone_factory ?>
-                    <br>
-                    <b><?= $factory->lang->getAttributeLabel('wc_email_factory') ?></b>: <?= $factory->lang->wc_email_factory ?>
-                    <br>
-                    <b><?= $factory->lang->getAttributeLabel('wc_contact_person_factory') ?></b>: <?= $factory->lang->wc_contact_person_factory ?>
-                    <br>
+                    <?php if (in_array(DOMAIN_TYPE, ['ru', 'ua', 'by'])) { ?>
+                        <b><?= $factory->lang->getAttributeLabel('wc_phone_supplier') ?></b>: <?= $factory->lang->wc_phone_supplier ?>
+                        <br>
+                        <b><?= $factory->lang->getAttributeLabel('wc_email_supplier') ?></b>: <?= $factory->lang->wc_email_supplier ?>
+                        <br>
+                        <b><?= $factory->lang->getAttributeLabel('wc_contact_person_supplier') ?></b>: <?= $factory->lang->wc_contact_person_supplier ?>
+                        <br>
+                    <?php } else { ?>
+                        <b><?= $factory->lang->getAttributeLabel('wc_phone_factory') ?></b>: <?= $factory->lang->wc_phone_factory ?>
+                        <br>
+                        <b><?= $factory->lang->getAttributeLabel('wc_email_factory') ?></b>: <?= $factory->lang->wc_email_factory ?>
+                        <br>
+                        <b><?= $factory->lang->getAttributeLabel('wc_contact_person_factory') ?></b>: <?= $factory->lang->wc_contact_person_factory ?>
+                        <br>
+                    <?php } ?>
                 <?php } ?>
-
             </div>
         </div>
     </div>
