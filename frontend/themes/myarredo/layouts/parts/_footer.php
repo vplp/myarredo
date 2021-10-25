562c19cb8278d0f634b642fb3246dbe4ee4e0efd
andrii bondarchuk <zndron@gmail.com>
2021-10-25 16:50:11 +0300

    upd

--- frontend/themes/myarredo/layouts/parts/_footer.php
+++ frontend/themes/myarredo/layouts/parts/_footer.php
@@ -100,7 +100,7 @@ $url = Url::to(['/forms/forms/ajax-get-form-feedback'], true);
 
                     </div>
 
-                    <?php if ($this->beginCache('Footer' . Yii::$app->city->getCityId() . Yii::$app->language, ['duration' => 7200])) { ?>
+                    <?php //if ($this->beginCache('Footer' . Yii::$app->city->getCityId() . Yii::$app->language, ['duration' => 7200])) { ?>
                         <ul class="nav ftr-nav">
                             <li><?= FormFeedback::widget(['view' => 'ajax_form_feedback']); ?></li>
                             <?php if (in_array(Yii::$app->language, ['ru-RU', 'uk-UA'])) { ?>
@@ -156,7 +156,7 @@ $url = Url::to(['/forms/forms/ajax-get-form-feedback'], true);
                             </div>
                         </div>
                         <?php
-                        $this->endCache();
+                        //$this->endCache();
                     } ?>
                 </div>
             </div>
