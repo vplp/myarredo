7102908f5862b332d1b037dfbe9d44195344c66d
andrii bondarchuk <zndron@gmail.com>
2021-10-27 22:19:21 +0300

    upd

--- thread/widgets/editors/tinymce/assets/filemanager/config/config.php
+++ thread/widgets/editors/tinymce/assets/filemanager/config/config.php
@@ -33,9 +33,9 @@ if (!isset($_SESSION['TINYMCE_filemanager_ALLOW']) || $_SESSION['TINYMCE_fileman
 
 $base_url = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'];  // DON'T TOUCH (base url (only domain) of site (without final /)).
 //$base_url = "http://localhost/!y2";  // DON'T TOUCH (base url (only domain) of site (without final /)).
-$upload_dir = '/uploads/'; // path from base_url to base of upload folder (with start and final /)
+$upload_dir = '/uploads/tinymce/'; // path from base_url to base of upload folder (with start and final /)
 
-$current_path = '../../../../../../web/uploads/'; // relative path from filemanager folder to upload folder (with final /)
+$current_path = '../../../../../../web/uploads/tinymce/'; // relative path from filemanager folder to upload folder (with final /)
 //thumbs folder can't put inside upload folder
 $thumbs_base_path = $current_path . 'thumbs/'; // relative path from filemanager folder to thumbs folder (with final /)
 
