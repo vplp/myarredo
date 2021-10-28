ae4625a157ce26dfb93d8f3cf3b14b8ca3422f2c
andrii bondarchuk <zndron@gmail.com>
2021-10-28 13:02:01 +0300

    upd

--- web/backend/js2/filemanager/config/include_yii.php
+++ web/backend/js2/filemanager/config/include_yii.php
@@ -8,6 +8,7 @@ $baseDirExecPath = dirname(__DIR__, 5);
 //
 if (session_id() == '') session_start();
 //
+/* !!! */ echo  '<pre style="color:red;">'; print_r($_SESSION); echo '</pre>'; /* !!! */
 if (!isset($_SESSION['TINYMCE_filemanager_ALLOW']) && $_SESSION['TINYMCE_filemanager_ALLOW'] !== true) {
     throw new Exception('Permission deny', 403);
 }
